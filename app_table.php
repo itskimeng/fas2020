814394b0e5d6df53ae15d3d663bf3f627ea1b42a
KimPogi <markkimsacluti10101996@gmail.com>
2021-02-15 16:25:54 +0800

    set id dropdown item

--- app_table.php
+++ app_table.php
@@ -31,7 +31,7 @@
                 ?>
                   <div class = "col-lg-2">
                   <label>Date</label>
-                    <select class="form-control select2" style= "color:blue;text-align:center;" id = "  " >
+                    <select class="form-control select2" style= "color:blue;text-align:center;" id = "dropdown1" >
                       <?php getDateE();?>
                     </select> 
                   </div>
