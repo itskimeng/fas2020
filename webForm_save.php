8531d8432c4a6f70f2f3daa9facef0fb72a9a4ba
KimPogi <markkimsacluti10101996@gmail.com>
2021-02-15 13:16:56 +0800

    commit changes on webForm save

--- webForm_save.php
+++ webForm_save.php
@@ -30,16 +30,7 @@ function submitWebPosting($category)
    
 
     if (mysqli_query($conn, $sql)) {
-        $file_name  = $_FILES['file']['name'];  
-        $temp_name  = $_FILES['file']['tmp_name'];  
-        if(isset($name) and !empty($name)){
-            $location = 'gdrive/';      
-            if(move_uploaded_file($temp_name, $location.$name)){
-                echo 'File uploaded successfully';
-            }
-        } else {
-            echo 'You should select a file to upload !!';
-        }
+       
     } else {
     }
 }
