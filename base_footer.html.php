df47c41b0ba65738962077d8722b28acfd8cd3ee
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-15 16:50:01 +0800

    added base file

---
+++ base_footer.html.php
@@ -0,0 +1,6 @@
+<footer class="main-footer"><br>
+	<div class="pull-right hidden-xs">
+	  <b>Version</b> 1.0
+	</div>
+	<strong>DILG IV-A Regional Information and Communications Technology Unit (RICTU) © <?php echo date('Y'); ?> All Right Reserved .</strong> 
+</footer>
