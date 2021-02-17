33337eaed748c2cce45200852c40069e820bad05
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-17 13:52:03 +0800

    test code refactor

--- base_menu.html.php
+++ base_menu.html.php
@@ -1,20 +1,23 @@
 <!-- should only call once -->
-
-
 <?php 
   session_start();
   if(!isset($_SESSION['username'])){
   header('location:index.php');
   }else{
     error_reporting(0);
-  ini_set('display_errors', 0);
-  $username = $_SESSION['username'];
-  $division = $_GET['division'];
-  $OFFICE_STATION = $_SESSION['OFFICE_STATION'];
+    ini_set('display_errors', 0);
+    $username = $_SESSION['username'];
+    $division = $_GET['division'];
+    $OFFICE_STATION = $_SESSION['OFFICE_STATION'];
   }
 ?>
 
-  <?php echo $OFFICE_STATION; ?>
+<?php 
+  print_r($OFFICE_STATION);
+  echo "<br>";
+  print_r('test code');
+  die();
+?>
 
 <?php include('connection.php');?> 
 <!--  -->
