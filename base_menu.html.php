2f9ed8f4605d52ef909b97ac2131dd0e85d04f67
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-17 15:18:39 +0800

    test code refactor

--- base_menu.html.php
+++ base_menu.html.php
@@ -1,33 +1,4 @@
-<?php 
-  session_start();
-
-  if (!isset($_SESSION['username'])) {
-    header('location:index.php');
-  } else {
-    error_reporting(0);
-    ini_set('display_errors', 0);
-    $admins = ['charlesodi', 'mmmon iro', 'cvferrer', 'masacluti', 'seolivar'];
-    $data['username'] = $_SESSION['username'];
-    $data['division'] = $_GET['division'];
-    $data['DEPT_ID'] = $_SESSION['DEPT_ID'];
-    $data['OFFICE_STATION'] = $_SESSION['OFFICE_STATION'];
-    $data['is_admin'] = false;
-
-    if (in_array($_SESSION['username'], $admins)) {
-      $data['is_admin'] = true;
-    }
-    
-    
-  }
-
-?>
-
-<?php 
-  print_r($_SESSION); 
-  echo "<br>";
-  echo "test code";
-  die();
-?>
+<?php include('base_call_connect.php');?> 
 <?php include('connection.php');?> 
 <!--  -->
 
