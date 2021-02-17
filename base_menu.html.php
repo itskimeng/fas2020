eb862b31eb531e9c23f6b7c66c9c2b41992fddaa
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-17 14:30:26 +0800

    code refactor

--- base_menu.html.php
+++ base_menu.html.php
@@ -1,16 +1,16 @@
 <!-- should only call once -->
 
+<?php include('connection.php');?> 
 
 <?php 
   session_start();
 
-  print_r($_SESSION);
+  print_r($_SESSION['username']);
   echo "<br>";
   print_r('test code');
   die();
 ?>
 
-<?php include('connection.php');?> 
 <!--  -->
 
 <!DOCTYPE html>
