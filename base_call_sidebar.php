16fe15be7fae8421b8ecbd26db68557c617983d2
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-17 10:19:28 +0800

    code refactor

--- base_call_sidebar.php
+++ base_call_sidebar.php
@@ -3,7 +3,7 @@ session_start();
 
 if ($data['is_admin']) { 
 	$path = 'test1.php'; 
-} elseif ($data['OFFICE_STATION'] == 1) {
+} elseif ($data['OFFICE_STATION'] == 1 || $data['OFFICE_STATION'] == "1") {
 	$path = 'sidebar2.php';     
 } else {
 	$path = 'sidebar3.php';
