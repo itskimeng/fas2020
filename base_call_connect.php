f76f9d4bcbcb1f76c9188e703c454e065b0976a0
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-17 10:47:55 +0800

    test code refactor

--- base_call_connect.php
+++ base_call_connect.php
@@ -2,12 +2,17 @@
 session_start();
 
 if (!isset($_SESSION['username'])) {
+	print_r('walang user');
+	die();
 	header('location:index.php');
+
 } else {
 	error_reporting(0);
 	ini_set('display_errors', 0);
 	$admins = ['charlesodi', 'mmmonteiro', 'cvferrer', 'masacluti', 'seolivar'];
 
+	print_r($_SESSION);
+	die();
 	$data['username'] = $_SESSION['username'];
 	$data['division'] = $_GET['division'];
 	$data['DEPT_ID'] = $_SESSION['DEPT_ID'];
