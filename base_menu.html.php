0b15227cbcb5486f6b2ea49fb5237e1865fff173
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-02-17 15:09:02 +0800

    test code

--- base_menu.html.php
+++ base_menu.html.php
@@ -1,18 +1,35 @@
 <!-- should only call once -->
 
-<?php include('connection.php');?> 
-
-<?php
+<?php 
   session_start();
 
-  print_r($conn);
-  echo "<br>";
-  print_r($_SESSION['username']);
-  echo "<br>";
-  print_r('test code');
-  die();
+  if (!isset($_SESSION['username'])) {
+    header('location:index.php');
+  } else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+    $admins = ['charlesodi', 'mmmon iro', 'cvferrer', 'masacluti', 'seolivar'];
+    $data['username'] = $_SESSION['username'];
+    $data['division'] = $_GET['division'];
+    $data['DEPT_ID'] = $_SESSION['DEPT_ID'];
+    $data['OFFICE_STATION'] = $_SESSION['OFFICE_STATION'];
+    $data['is_admin'] = false;
+
+    if (in_array($_SESSION['username'], $admins)) {
+      $data['is_admin'] = true;
+    }
+    
+    return $data;
+  }
+
 ?>
 
+<?php 
+  echo $data; 
+  echo "<br>";
+  echo "test code";
+?>
+<?php include('connection.php');?> 
 <!--  -->
 
 <!DOCTYPE html>
