81d552e311f971dec026a611406910356c0cbfd1
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-03-08 08:35:47 +0800

    code refactor and include alert message element

--- ActivityPlanner/views/index.html.php
+++ ActivityPlanner/views/index.html.php
@@ -2,14 +2,14 @@
   require_once 'ActivityPlanner/controller/ActivityMonitoringController.php';
 ?>
 
-
 <div class="content-wrapper">
     <section class="content-header">
-      <!-- <button><a href="update_series_code.php">Run</a></button> -->
-        
         <h1>
           LGCDD Activity Planner
         </h1>
+        
+        <?php include('alert_message.html.php'); ?>
+
         <ol class="breadcrumb"> 
           <li>
             <a href="home.php">
@@ -17,8 +17,11 @@
               Home
             </a>
           </li> 
+          <li>
+            <a href="#">LGCDD</a>
+          </li>
           <li class="active">
-            LGCDD Activity Planner
+            Activity Planner
           </li>
         </ol> 
     </section>
@@ -42,11 +45,188 @@
 <?php include('css.html.php');?>
 <?php include('modal_edit.html.php'); ?>
 <?php include('modal_delete.html.php'); ?>
-<?php include('js.html.php'); ?>
 
+<script type="text/javascript">
+  function generateEventData($data) {
+    let modal = $('#edit_modal');
+    let elements = ['event_id','emp_id','title','act_status','event_code','target_participants','description','collaborators','priority', 'profile', 'host'];
 
+    $.each(elements, function(key, val){
+        let el = modal.find('#cform-'+val);
 
-
-
+        switch(key) {
+          case 7:
+            el.val($data[val]);
+            el.select2();
+            break;
+          case 9:
+            el.attr('src', $data[val]);
+            break;
+          case 10:
+            el.append($data[val]);  
+            break;
+          default:
+            el.val($data[val]);
+        }
+    });
     
+    let daterange = modal.find('#daterange-btn');
+    let date_from = modal.find('#cform-date_from');  
+    let date_to = modal.find('#cform-date_to');
 
+    for (let $i=1; $i<=$data.priority; $i++) {
+      let star = $('#edit_modal .rate'+$i);
+      star.addClass('active-star');
+      star.css('color', 'gold');
+    }
+
+    if ($data.status == "Finished") {
+      modal.find('save_changes').addClass('hidden');
+    }
+
+
+    let date_start = $data.date_start;
+    let date_end = $data.date_end;
+
+    if ($data.is_new > 0) {
+      daterange.html(date_start.format('MMMM D, YYYY') + ' - ' + date_end.format('MMMM D, YYYY'));
+      date_from.val(date_start.format('YYYYMMDD hh:mm a'));
+      date_to.val(date_end.format('YYYYMMDD hh:mm a'));
+    } else {
+      daterange.html(date_start.format('MMMM D, YYYY hh:mm a') + ' - ' + date_end.format('MMMM D, YYYY hh:mm a'));
+      date_from.val(date_start.format('YYYYMMDD hh:mm a'));
+      date_to.val(date_end.format('YYYYMMDD hh:mm a'));
+    }
+
+    //Date range as a button
+    // on change
+    daterange.daterangepicker({
+       timePicker: true, timePickerIncrement: 30, locale: { format: 'MMMM D, YYYY hh:mm A' },
+        ranges   : {
+          'Today'       : [moment(), moment()],
+          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        // startDate: moment().subtract(29, 'days'),
+        endDate  : moment()
+      },
+      function (start, end) {
+        daterange.html(start.format('MMMM D, YYYY hh:mm A') + ' - ' + end.format('MMMM D, YYYY hh:mm A'));
+        date_from.val(start.format('YYYYMMDD hh:mm a'));
+        date_to.val(end.format('YYYYMMDD hh:mm a'));
+      }
+    );
+
+  }
+
+  function highlightStar($pointer, $active=false) {
+    for (let $i=0; $i<=5; $i++) {
+      if ($i <= $pointer) {
+        $('#edit_modal .rate'+$i).css('color', 'gold');
+        $('#edit_modal .rate'+$i).css('transform', 'scale(1.1)');
+        if ($active) {
+          $('#edit_modal .rate'+$i).addClass('active-star');
+        }
+      } else if (!$active) {
+        if (!$('#edit_modal .rate'+$i).hasClass('active-star')) {
+          $('#edit_modal .rate'+$i).css('color', '#ddd');
+          $('#edit_modal .rate'+$i).css('transform', '');
+          $('#edit_modal .rate'+$i).removeClass('active-star');
+        }
+      } else {
+          $('#edit_modal .rate'+$i).css('color', '#ddd');
+          $('#edit_modal .rate'+$i).css('transform', '');
+          $('#edit_modal .rate'+$i).removeClass('active-star');
+      }  
+    }  
+  }
+
+  function unhighlightStar() {
+    for (let $i=0; $i<=5; $i++) {
+      if (!$('#edit_modal .rate'+$i).hasClass('active-star')) {
+        $('#edit_modal .rate'+$i).css('color', '#ddd');
+        $('#edit_modal .rate'+$i).css('transform', '');
+      }  
+    }  
+  }
+
+  function clearStar() {
+    for (let $i=0; $i<=5; $i++) {
+      $('#edit_modal .rate'+$i).css('color', '#ddd');
+      $('#edit_modal .rate'+$i).removeClass('active-star');
+      $('#edit_modal .rate'+$i).css('transform', '');  
+    }   
+  }
+
+  function deleteEvent(el) {
+    let tr = $(el).closest('tr');
+    let $id = tr.find('.act_id');
+    let modal = $('#delete_modal');
+    let event_id = modal.find('#cform-delete_event_id');
+
+    console.log($id.val());
+    event_id.val($id.val());
+  }
+
+  $(document).ready(function() {    
+    $('.daterange').daterangepicker();
+    let colab = $('#edit_modal').find('#cform-collaborators');
+    colab.select2();
+
+    $('#edit_modal .fa-star').click(function() {
+      let num = $(this).attr('value');
+      highlightStar(num, true);
+      $('#edit_modal #cform-priority').val(num);
+    });
+
+    $('#edit_modal .fa-star').hover(function() {
+      highlightStar($(this).attr('value'));
+    });
+
+    $('#edit_modal .rate').mouseout(function() {
+      unhighlightStar(5);
+    });
+
+    $(document).on('click', '.edit_activity', function() {
+      let  $data = [];
+      let tr = $(this).closest('tr');
+      let act_collaborators = tr.find('.act_collaborators').val();
+
+      $data = {
+        event_code: tr.find('.act_code').val(),
+        event_id: tr.find('.act_id').val(),
+        emp_id: tr.find('.emp_id').val(),
+        title: tr.find('.act_title').val(),
+        host: tr.find('.host').val(),
+        profile: tr.find('.profile').val(),
+        act_status: tr.find('.act_status').val(),
+        date_start: moment(tr.find('.date_start').val()),
+        date_end: moment(tr.find('.date_end').val()),
+        description: tr.find('.description').val(),
+        priority: tr.find('.act_priority').val(),
+        is_new: tr.find('.is_new').val(),
+        target_participants: tr.find('.target_participants').val(),
+        collaborators: JSON.parse(act_collaborators)
+      };
+
+      generateEventData($data);
+    });
+
+    // $('.delete_activity').click(function() {
+    //   let  $data = [];
+    //   let tr = $(this).closest('tr');
+    //   let id = tr.find('.act_id').val()
+      
+    //   // deleteEvent(id);
+    // });
+
+    $('#edit_modal').on('hidden.bs.modal', function () {
+      $('#edit_modal #cform-host').empty();
+      clearStar();
+    });
+
+  });
+</script>
