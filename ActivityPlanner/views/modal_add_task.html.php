02935c1c2d0a43b587147db5a19bd06d58c1232a
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-03-08 08:37:04 +0800

    code refactor, added new modal for adding task

---
+++ ActivityPlanner/views/modal_add_task.html.php
@@ -0,0 +1,29 @@
+<div class="modal fade" id="modal-add_task" tabindex="-1" role="dialog" aria-labelledby="modal-add_task_modalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content activity_content">
+      <div class="modal-header activity_header" style="background-color: #f6cdd0;">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4><i class="fa fa-gears"></i> Create Task</h4>
+      </div>
+      <form method="POST" action="ActivityPlanner/entity/save_subtasks.php" >
+        <div class="modal-body">
+
+          <?php echo input_hidden('event_id','event_id', 'event_id', $_GET['event_planner_id']); ?>
+          <?php echo input_hidden('event_program','event_program', 'event_program', $event_data['event_program']); ?>
+          <?php echo input_hidden('current_user','current_user', 'current_user', $event_data['current_user']); ?>
+
+          <?php echo group_text('Title','subtask','', '',1, false,''); ?>
+          <?php echo group_select('Person','person',$collaborators, '','', 1, false, 1); ?>
+          <?php echo group_daterange3('Timeline', 'timeline', 'timeline', '', '', 'daterange ', 1, false); ?>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" name="submit" value="" class="btn btn-primary" onClick="closeEditModal()">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
