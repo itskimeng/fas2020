dc17fe5855fe69dfa65632d39329d2d03be46d8c
KimPogi <markkimsacluti10101996@gmail.com>
2021-03-12 09:24:10 +0800

    upload webposting template

--- report/WEBPOSTING/pages/viewTA.php
+++ report/WEBPOSTING/pages/viewTA.php
@@ -24,18 +24,61 @@ $conn=mysqli_connect('localhost','fascalab_2020','w]zYV6X9{*BN','fascalab_2020')
             
                 $name = '';
                 $result = mysqli_query($conn, $query);
+            
+        
+                $a = ["News","Procurement","Banner","Transparency","LGUs","Procurement","Vacancies","Photo","Video","Forms"];
+       
                 if($row = mysqli_fetch_array($result))
                 {
                     $reqby = $row['REQUESTED_BY'];
-                    $PHPJasperXML = new PHPJasperXML();
-                    $PHPJasperXML->arrayParameter=array(
-                        "requested_by"=>$reqby,
-  
-);
+                    $reqtime = $row['REQUESTED_TIME'];
+                    $reqdate = $row['REQUESTED_DATE'];
+                    $office = $row['OFFICE'];
+                    $position = $row['POSITION'];
+                    $mobile = $row['MOBILE_NO'];
+                    $purpose = $row['PURPOSE'];
+                    $attachements = $row['ATTACHMENT'];
+
+                    $time = new DateTime($reqtime);
+                    $daate = new DateTime($reqdate);
+                    if (in_array($row['CATEGORY'],$a))
+                        {                   
+                            $PHPJasperXML = new PHPJasperXML();
+                            $PHPJasperXML->arrayParameter=array(
+                            "requested_by"=>$reqby,
+                            "requested_time"=>$time->format('g:i A'),
+                            "requested_date"=>$time->format('F d, Y'),
+                            "office"=>$office,
+                            "position"=>$position,
+                            "mobile_no"=>$mobile,
+                            "purpose"=>$purpose,
+                            "category1"=>setParam("News",$row['CATEGORY']),
+                            "category2"=>setParam("Banner",$row['CATEGORY']),
+                            "category3"=>setParam("Transparency",$row['CATEGORY']),
+                            "category4"=>setParam("LGUs",$row['CATEGORY']),
+                            "category5"=>setParam("Procurement",$row['CATEGORY']),
+                            "category6"=>setParam("Vacancies",$row['CATEGORY']),
+                            "category7"=>setParam("Photo",$row['CATEGORY']),
+                            "category8"=>setParam("Video",$row['CATEGORY']),
+                            "category9"=>setParam("Forms",$row['CATEGORY']),
+                            "attachements"=>$
+                        );
+                        }else{
+
+                        }
+                        // print_r($PHPJasperXML);
+                }
+            
+                    function setParam($category,$val)
+                {
+                    if($category == $val)
+                    {
+                        return 'correct.png';
+                    }else{
+                        return '';
+                    }
                 }
     
-
-
 
 
 // $PHPJasperXML->debugsql=true;
@@ -44,7 +87,7 @@ $conn=mysqli_connect('localhost','fascalab_2020','w]zYV6X9{*BN','fascalab_2020')
 
 $PHPJasperXML->load_xml_file("view.jrxml");
 $PHPJasperXML->transferDBtoArray('localhost','fascalab_2020','w]zYV6X9{*BN','fascalab_2020');
-    $PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
+$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
 
 
 ?>
--- webForm_save.php
+++ webForm_save.php
@@ -37,7 +37,16 @@ function submitWebPosting($category)
     echo $sql;
 }
 submitWebPosting($_POST['chk_category']);
-    
+$file_name  = $_FILES['file']['name'];  
+$temp_name  = $_FILES['file']['tmp_name'];  
+if(isset($name) and !empty($name)){
+    $location = 'gdrive/';      
+    if(move_uploaded_file($temp_name, $location.$name)){
+        echo 'File uploaded successfully';
+    }
+} else {
+    echo 'You should select a file to upload !!';
+}
 
 
 ?>
