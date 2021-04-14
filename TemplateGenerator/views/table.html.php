51a9699a8ebd1f20cae2bc51ef6372f026b6e5b0
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-14 14:43:40 +0800

    update button icons

--- TemplateGenerator/views/table.html.php
+++ TemplateGenerator/views/table.html.php
@@ -4,29 +4,37 @@
 
 <div class="box">
 	<div class="box-header">
-		<div class="box-tool">
+		<!-- <div class="box-tool"> -->
+			<!-- <div class="margin" style="position:absolute; margin:0px">
 			<div class="btn-group">
             	<a href='base_tempgen_add_form.html.php?&username=<?php echo $_SESSION['username']; ?>&division=<?php echo $_GET['division']; ?>&emp_id=<?php echo $_SESSION['currentuser']; ?>' class="btn btn-block btn-primary"><i class="fa fa-plus"></i> Create New</a>  
             </div>
-		</div>
+        	</div> -->
+		<!-- </div> -->
 	</div>
 	<div class="box-body">
+		<div class="margin" style="position:absolute; margin:0px">
+			<div class="btn-group">
+            	<a href='base_tempgen_add_form.html.php?&username=<?php echo $_SESSION['username']; ?>&division=<?php echo $_GET['division']; ?>&emp_id=<?php echo $_SESSION['currentuser']; ?>' class="btn btn-block btn-primary"><i class="fa fa-plus"></i> Create New</a>  
+            </div>
+        </div>
+
 		<table id="list_table" class="table table-striped table-bordered table-responsive table-hover" role="grid">
 			<thead>
 				<tr>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:20%;">Type</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle;">Code</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%;">Title</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%;">Attendee</th>
-		              <th colspan="2" style = "text-align:center; vertical-align: middle;">Activity Date</th> 
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%;">Venue</th> 
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%;">Date Given</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%;">Date Generated</th>  
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:20%; color: white; background-color: #73758799; border-left: none; border-top-left-radius: 4px; -webkit-border-top-left-radius: 4px; -moz-border-radius-topleft: 4px;">Type</th>
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; color:black; color: white; background-color: #73758799;">Code</th>
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">Title</th>
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%; color:black; color: white; background-color: #73758799;">Attendee</th>
+		              <th colspan="2" style = "text-align:center; vertical-align: middle; color:black; color: white; background-color: #73758799;">Activity Date</th> 
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%; color:black; color: white; background-color: #73758799;">Venue</th> 
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">Date Given</th>
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799; border-right: none; border-top-right-radius: 4px; -webkit-border-top-right-radius: 4px; -moz-border-radius-topright: 4px;">Date Generated</th>  
 
 		            </tr>
 		            <tr>
-		              <th style="text-align: center; vertical-align: middle; width:10%;">From</th>
-		              <th style="text-align: center; vertical-align: middle; width:10%;">To</th>
+		              <th style="text-align: center; vertical-align: middle; width:10%; color: white; background-color: #73758799;">From</th>
+		              <th style="text-align: center; vertical-align: middle; width:10%; color: white; background-color: #73758799;">To</th>
 		            </tr>
 			</thead>
 			<tbody id="list_body">
