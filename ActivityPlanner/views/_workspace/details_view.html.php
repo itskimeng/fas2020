89563d0ca8dddcd9e6a6b4049f5da31ef42fe3f0
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-14 13:48:01 +0800

    code refactor

--- ActivityPlanner/views/_workspace/details_view.html.php
+++ ActivityPlanner/views/_workspace/details_view.html.php
@@ -1,7 +1,3 @@
-<!-- <?php 
-  //require_once '..\..\views\macro.html.php';
-?> -->
-
 <div class="col-md-2">
   <img class="profile-user-img img-responsive img-circle" src="images/logo.png" alt="User profile picture">
         <h6 class="text-center host_name"><b>Host</b></h6>
@@ -9,40 +5,35 @@
 </div>
 <div class="col-md-3">
   <!-- title -->
-    <!-- <?php //echo group_text('Title','title','', '', 1, false,'title'); ?> -->
-    <div id="cgroup-title" class="form-group">
-      <label class="control-label">Title</label><br>
-      <input id="cform-title" placeholder="Title" type="text" name="title" class="form-control title" value="" required novalidate />
-    </div>  
-    <!-- venue -->
-    <!-- <?php //echo group_text('Venue','venue','', '', 1, false,'venue'); ?> -->
-    <div id="cgroup-venue" class="form-group">
-      <label class="control-label">Venue</label><br>
-      <input id="cform-venue" placeholder="venue" type="text" name="venue" class="form-control venue" value="" required novalidate />
-    </div>  
+  <div id="cgroup-title" class="form-group">
+    <label class="control-label">Title</label><br>
+    <input id="cform-title" placeholder="Title" type="text" name="title" class="form-control title" value="" required novalidate />
+  </div>  
+  <!-- venue -->
+  <div id="cgroup-venue" class="form-group">
+    <label class="control-label">Venue</label><br>
+    <input id="cform-venue" placeholder="venue" type="text" name="venue" class="form-control venue" value="" required novalidate />
+  </div>  
 </div>
 <div class="col-md-3">
   <!-- description -->
-        <!-- <?php //echo group_textarea('Description','description',''); ?>  -->
-        <div class="form-group">
-          <label>Description</label>
-          <textarea id="cform-description" name="description" class="form-control description" rows="3" placeholder="Description" required>
-          </textarea>
-        </div>  
+  <div class="form-group">
+    <label>Description</label>
+    <textarea id="cform-description" name="description" class="form-control description" rows="3" placeholder="Description" required>
+    </textarea>
+  </div>  
 </div>
 <div class="col-md-3">
   <!-- title -->
-    <!-- <?php //echo group_text('Date Start','date_start','', '', 1, false,'date_start'); ?> -->
-    <div id="cgroup-date_start" class="form-group">
-      <label class="control-label">Date Start</label><br>
-      <input id="cform-date_start" placeholder="date_start" type="text" name="date_start" class="form-control date_start" value="" required novalidate />
-    </div>  
-    <!-- description -->
-        <!-- <?php //echo group_text('Date End','date_end','', '', 1, false,'date_end'); ?> -->
-    <div id="cgroup-date_end" class="form-group">
-      <label class="control-label">Date End</label><br>
-      <input id="cform-date_end" placeholder="date_end" type="text" name="date_end" class="form-control date_end" value="" required novalidate />
-    </div>  
+  <div id="cgroup-date_start" class="form-group">
+    <label class="control-label">Date Start</label><br>
+    <input id="cform-date_start" placeholder="date_start" type="text" name="date_start" class="form-control date_start" value="" required novalidate />
+  </div>  
+  <!-- description -->
+  <div id="cgroup-date_end" class="form-group">
+    <label class="control-label">Date End</label><br>
+    <input id="cform-date_end" placeholder="date_end" type="text" name="date_end" class="form-control date_end" value="" required novalidate />
+  </div>  
 </div>
 <div class="col-md-3">
   
--- ActivityPlanner/views/_workspace/filters_view.html.php
+++ ActivityPlanner/views/_workspace/filters_view.html.php
@@ -1,19 +1,116 @@
 <?php 
-	
-	require_once 'ActivityPlanner/views/macro.html.php';
-	require_once 'ActivityPlanner/manager/ActivityPlanner.php';
 
-	$ap = new ActivityPlanner();
+	function group_select($label, $name, $options, $value, $class, $label_size, $readonly=false, $body_size=1, $required=true) {
+		$element = '<div id="cgroup-'.$name.'" class="form-group">';
+		if ($label_size > 0) {
+			$element .= '<label class=" control-label">'.$label.':</label><br>';
+		}
+
+	    if ($readonly) {
+		   $element .= '<select id="cform-'.$name.'" name="'.$name.'" class="form-control select2 '.$class.'" data-placeholder="-- Select '.$label.' --" disabled>';
+	       // $element .= '<input type="hidden" name="hidden-'.$name.'" value="'.$value.'" />'
+	    } else {
+	       $element .= '<select id="cform-'.$name.'" name="'.$name.'" class="form-control select2 '.$class.'" data-placeholder="-- Select '.$label.' --" required="'.$required.'">'; 
+	    }
+
+		$element .= group_options($options, $value);
+
+	    $element .= '</select>';
+		$element .= '<input type="hidden" name="hidden-'.$name.'" value="'.$value.'" />';
+		$element .= '</div>';
+
+		return $element;
+	}
+
+	function group_options($fields, $selected) {
+	    $element = '<option disabled selected></option>';
+	    foreach ($fields as $key=>$value) {
+	        if ($key == $selected) {
+	            $element .= '<option value="'.$key.'" selected="selected">'.$value.'</option>';
+	        } else {
+	            $element .= '<option value="'.$key.'">'.$value.'</option>';
+	        }
+	    }
+	    
+	    return $element;
+	}
+
+	function fetchPrograms() {
+        $conn = mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
+
+        $programs = [];
+
+        $sql = "SELECT id, code, name FROM event_programs"; 
+        $query = mysqli_query($conn, $sql);
+        
+        $programs['ALL'] = 'ALL';
+        while ($row = mysqli_fetch_assoc($query)) {
+            $programs[$row['code']] = $row['code'];
+        }
+
+        return $programs;
+    }
+
+    function fetchActivities() {
+        $conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
+        $data = [];
+        $sql = "SELECT 
+                    events.id as id, 
+                    events.title as title,
+                    events.program as program,
+                    events.code_series as code
+                FROM events
+                LEFT JOIN tblpersonneldivision tp on tp.DIVISION_N = events.office
+                LEFT JOIN tblemployeeinfo te on te.EMP_N = events.postedby
+                LEFT JOIN tbldesignation tbl_desg on tbl_desg.DESIGNATION_ID = te.DESIGNATION
+                    WHERE tp.DIVISION_M like '%CDD%'
+                    ORDER BY events.id DESC";
+
+        $query = mysqli_query($conn, $sql);
+
+        while ($row = mysqli_fetch_assoc($query)) {
+            $data[$row['id']] = $row['code'] .' : '.$row['title'];
+        } 
+
+        return $data;
+    }
+
+    function group_daterange3 ($label, $id, $name, $value_from, $value_to, $class, $label_size=1, $is_readonly=false, $format_display = 'm/d/Y') {
+
+	    $element = '<div class="form-group">';
+
+	    if ($label_size > 0) {
+	        $element .= '<label>'.$label.':</label>';
+	    }
+
+	    $element .= '<div class="input-group">';
+	    $element .= '<div class="input-group-addon input-sm">';
+	    $element .= '<i class="fa fa-calendar"></i>';
+	    $element .= '</div>';
+	    if (!$is_readonly) {
+	        $element .= '<input type="text" name="'.$name.'" class="form-control pull-right '.$class.'" placeholder="'.$label.'" value="'.(empty($value_from)?date($format_display):$value_from).' - '.(empty($value_to)?date($format_display):$value_to).'" id="'.$id.'">';
+	    } else {
+	        $element .= '<input type="text" name="'.$name.'" class="form-control pull-right '.$class.'" placeholder="'.$label.'" value="'.(empty($value_from)?date($format_display):$value_from).' - '.(empty($value_to)?date($format_display):$value_to).'" id="'.$id.'" disabled>';  
+
+	        $input_hidden = input_hidden('timeline', 'timeline[]', 'timeline', $value_from .'-'. $value_to);
+	        
+	        echo $input_hidden;
+	    }
+	    $element .= '</div>';
+	    $element .= '</div>';
+
+	    return $element;
+	}
 ?>
 
 <!-- program -->
 <div class="col-md-4">
-	<?php echo group_select('Program', 'filter_program', $ap->fetchPrograms(), 'ALL', 'filter_program', 1, false) ?>
+	<?php echo group_select('Program', 'filter_program', fetchPrograms(), 'ALL', 'filter_program', 1, false) ?>
 </div>
 
 <!-- title -->
 <div class="col-md-4">
-	<?php echo group_select('Title', 'filter_title', $ap->fetchActivities(), '', 'filter_title', 1, false) ?>
+	<?php echo group_select('Title', 'filter_title', fetchActivities(), '', 'filter_title', 1, false) ?>
 </div>
 
 <!-- timeline -->
