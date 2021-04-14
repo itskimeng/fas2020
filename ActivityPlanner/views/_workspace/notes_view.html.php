ce2a0c9bfcd9f19910fd82eef6bf7d0c5987d78d
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-14 13:49:46 +0800

    remove path due to conflict

--- ActivityPlanner/views/_workspace/notes_view.html.php
+++ ActivityPlanner/views/_workspace/notes_view.html.php
@@ -1,11 +1,3 @@
-<?php 
-	
-	require_once '..\..\views\macro.html.php';
-	require_once '..\..\manager\ActivityPlanner.php';
-
-	$ap = new ActivityPlanner();
-?>
-
 <!-- program -->
 <div class="col-md-12">
 	<div id="note_box" class="form-group note_box" style="overflow-y: scroll; max-height: 151px!important; min-height: 151px!important;">
