6a3725dddb4deddc3056be7025f3b021cf28da18
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-15 21:46:32 +0800

    code refactor

--- base_menu.html.php
+++ base_menu.html.php
@@ -1,7 +1,7 @@
 <?php require_once 'bower_components/phpti-master/src/ti.php'; ?>
 
-<?php include('base_call_connect.php');?> 
-<?php include('connection.php');?> 
+<?php include('base_call_connect.php'); ?> 
+<?php include('connection.php'); ?> 
 
 <!DOCTYPE html>
 <html>
@@ -9,11 +9,11 @@
     <link rel="shortcut icon" type="image/png" href="dilg.png">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>FAS | <?php emptyblock('title') ?></title>
+    <title>FAS | <?php emptyblock('title'); ?></title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     
     <!-- all sets will be loaded here -->
-    <?php startblock('assets') ?>
+    <?php startblock('assets'); ?>
       <?php include 'base_menu.css.php'; ?>
     
       <script src="calendar/fullcalendar/lib/jquery.min.js"></script>
@@ -26,20 +26,20 @@
 
   </head>
   <body >
-    <?php startblock('sidebar') ?>
+    <?php startblock('sidebar'); ?>
       <?php include 'base_call_sidebar.php'; ?>
-    <?php endblock() ?>
+    <?php endblock(); ?>
 
     <div class="wrapper">
       <!-- all contents will be included here -->
-      <?php emptyblock('content') ?>
+      <?php emptyblock('content'); ?>
       <!-- end all -->
     </div>
     
     <div id="footer">
-      <?php startblock('footer') ?>
+      <?php startblock('footer'); ?>
         <?php include 'base_footer.html.php'; ?>
-      <?php endblock() ?>
+      <?php endblock(); ?>
     </div>
     </body>
 </html>
