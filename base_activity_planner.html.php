37d62bac4ede98e1a4e3eb38d15b8d3dfdd9c0dd
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-15 21:07:21 +0800

    added new template file

---
+++ base_activity_planner.html.php
@@ -0,0 +1,10 @@
+<?php include 'base_menu.html.php'; ?>
+
+<?php startblock('title') ?>
+  Activity Planner
+<?php endblock('title') ?>
+
+<?php startblock('content') ?>
+  <?php include('ActivityPlanner/views/macro.html.php'); ?>
+  <?php include('ActivityPlanner/views/index.html.php'); ?>
+<?php endblock() ?>
