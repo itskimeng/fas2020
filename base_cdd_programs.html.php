cbfdd1c96fda2a85bebb82b8ea955a51e96b8f13
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-15 20:55:25 +0800

    code optimization

---
+++ base_cdd_programs.html.php
@@ -0,0 +1,10 @@
+<?php include 'base_menu.html.php'; ?>
+
+<?php startblock('title') ?>
+  LGCDD Programs
+<?php endblock('title') ?>
+
+<?php startblock('content') ?>
+  <?php include('ActivityPlanner/views/macro.html.php'); ?>
+  <?php include('LGCDDProgram/index.html.php'); ?>
+<?php endblock() ?>
