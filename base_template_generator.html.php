e962f2bf2d18d530eae01b1130105ef6d0ddcc9f
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-22 18:01:58 +0800

    code refactor

--- base_template_generator.html.php
+++ base_template_generator.html.php
@@ -1,3 +1,7 @@
+<?php include('base_call_connect.php'); ?> 
+<?php include('connection.php'); ?> 
+<?php require_once 'bower_components/phpti-master/src/ti.php'; ?>
+
 <?php require_once 'menu_checker.php'; ?>
 <?php $menuchecker = menuChecker('template_generator'); ?>
 
