059e88ce3dd0bfaadaf98c3a0a4153e5cb328e30
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-04-23 11:38:59 +0800

    move path

--- base_planner_subtasks.html.php
+++ base_planner_subtasks.html.php
@@ -1,3 +1,10 @@
+<?php include('base_call_connect.php'); ?> 
+<?php include('connection.php'); ?> 
+<?php require_once 'bower_components/phpti-master/src/ti.php'; ?>
+
+<?php require_once 'menu_checker.php'; ?> 
+<?php $menuchecker = menuChecker('activity_planner'); ?> 
+
 <?php include 'base_menu.html.php'; ?>
 
 <?php startblock('title') ?>
