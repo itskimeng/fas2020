3a6c24e1b2744811bab353cfb801a946b87a4ef3
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-05-04 18:32:47 +0800

    added new certificate type

--- TemplateGenerator/entity/template.php
+++ TemplateGenerator/entity/template.php
@@ -6,7 +6,6 @@ require_once('../../tcpdfv02/tcpdf.php');
 require_once "../../connection.php";
 require_once '../manager/TemplateGenerator.php';
 
-
 $certificate_type = $_POST['certificate_type'];
 $attendees[] = $_POST['attendee'];
 $activity_title = $_POST['activity_title'];
@@ -37,7 +36,7 @@ $date_to = $date_to->format('F d, Y');
 $date_given_day = $date_given->format('jS');
 $date_given_my = $date_given->format('F Y');
 $template = new TemplateGenerator();
-
+$type = 'a';
 
 if ($_FILES['uploadfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['uploadfile']['tmp_name'])) { 
 	$attendee = file_get_contents($_FILES['uploadfile']['tmp_name']); 
@@ -49,10 +48,23 @@ if ($certificate_type == 'cop') {
     $certificate_type = "CERTIFICATE OF PARTICIPATION";
 } elseif ($certificate_type == 'coa') {
     $certificate_type = "CERTIFICATE OF APPRECIATION";
+} elseif ($certificate_type == 'coc') {
+    $type = 'b';
+    $certificate_type = "CERTIFICATE OF COMPLETION";    
 } else {
     $certificate_type = '';
 }
-    // Extend the TCPDF class to create custom Header and Footer
+
+$details = [
+    'certificate_type' => $certificate_type,
+    'activity_title' => $activity_title,
+    'date_range' => $dates,
+    'activity_venue' => $activity_venue,
+    'date_given_day' => $date_given_day,
+    'date_given_my' => $date_given_my
+];
+
+if ($type == 'a') {
     class MYPDF extends TCPDF {
         //Page header
         public function Header() {
@@ -74,11 +86,36 @@ if ($certificate_type == 'cop') {
             $this->setPageMark();
         }
     }
-// }
+} else {
+    class MYPDF extends TCPDF {
+        //Page header
+        public function Header() {
+            // get the current page break margin
+            $bMargin = $this->getBreakMargin();
+            // get current auto-page-break mode
+            $auto_page_break = $this->AutoPageBreak;
+            // disable auto-page-break
+            $this->SetAutoPageBreak(false, 0);
+            // set bacground image
+            // $img_file = K_PATH_IMAGES.'image_demo.jpg';
+            $img_file = '../../images/template/COC.jpg';
+
+            // $this->Image(file, LEFT, RIGHT, WIDTH, HEIGHT, '', '', '', false, 300, '', false, false, 0);
+            $this->Image($img_file, 12, 1, 275, 198, '', '', '', false, 300, '', false, false, 0);
+            // restore auto-page-break status
+            $this->SetAutoPageBreak($auto_page_break, $bMargin);
+            // set the starting point for the page content
+            $this->setPageMark();
+        }
+    }
+}
+
 
 // create new PDF document
 $pdf = new MYPDF('Landscape', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
+
+// die();
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('DILG IV-A RICTU');
@@ -94,7 +131,7 @@ $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 // set margins
-$pdf->SetMargins(50, 5, 50);
+$pdf->SetMargins(10, 5, 10);
 $pdf->SetHeaderMargin(0);
 $pdf->SetFooterMargin(0);
 
@@ -116,30 +153,28 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 $pdf->SetFont('times', '', 48);
 
 foreach ($attendees as $attendee) {
-    $pdf->AddPage();
+    if (!empty($attendee)) {
+        $pdf->AddPage();
 
-    $html = '<br><br><div style="text-align:center; font-size:10pt;">This<br>
-    <b style="font-family:Trajan Pro Bold; font-weight:bold;font-size:29pt;">'.$certificate_type.'</b><br><br>
-    is hereby awarded to<br><br><br><div style="font-family:helvetica;font-weight:bold;font-size:35pt; text-align:center;">'.$attendee.'</div><br><div style="font-family:Verdana Regular;font-size:12pt; text-align:center;">in recognition of his/her active paritcipation during the conduct of the <br><b>'.$activity_title.'</b><br>held on '.$dates.' via '.$activity_venue.'.<br><br>Given this <b>'.$date_given_day.'</b> day of <b>'.$date_given_my.'.</b></div>
-    </div>';
+        $html = $template->generateContent($details, $attendee);
+        $pdf->writeHTML($html, true, false, true, false, ''); 
 
-    $pdf->writeHTML($html, true, false, true, false, ''); 
+        $data = [
+            'certificate_type' => $certificate_type,
+            'attendee' => $attendee,
+            'activity_title' => $activity_title,
+            'date_from' => $db_datefrom->format('Y-m-d 00:00:00'),
+            'date_to' => $db_dateto->format('Y-m-d 23:59:59'),
+            'activity_venue' => $activity_venue,
+            'date_given' => $db_dategiven->format('Y-m-d 00:00:00'),
+            'date_generated' => $date_today->format('Y-m-d')
+        ];
+        
+        $exist = $template->find($conn, $data);
 
-    $data = [
-        'certificate_type' => $certificate_type,
-        'attendee' => $attendee,
-        'activity_title' => $activity_title,
-        'date_from' => $db_datefrom->format('Y-m-d 00:00:00'),
-        'date_to' => $db_dateto->format('Y-m-d 23:59:59'),
-        'activity_venue' => $activity_venue,
-        'date_given' => $db_dategiven->format('Y-m-d 00:00:00'),
-        'date_generated' => $date_today->format('Y-m-d')
-    ];
-    
-    $exist = $template->find($conn, $data);
-
-    if (empty($exist)) {
-        $template->insert($conn, $data); 
+        if (empty($exist)) {
+            $template->insert($conn, $data); 
+        }
     }
 }
 
@@ -154,6 +189,9 @@ function getCertType($type)
         case 'cop':
             $cert = 'base_template.jpg';
             break;
+        case 'coc':
+            $cert = 'COC.jpg';
+            break;    
         
         default:
             # code...
@@ -161,4 +199,8 @@ function getCertType($type)
     }
 
     return $cert;
+}
+
+function generateTex($type) {
+
 }
