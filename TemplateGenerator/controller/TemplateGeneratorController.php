4393b6ecdb27d6db5b3b9aa5757bfff7f4a4be0b
Jomarie Sodsod <sodsodjomarie@gmail.com>
2021-05-05 22:24:43 +0800

    code refactor

--- TemplateGenerator/controller/TemplateGeneratorController.php
+++ TemplateGenerator/controller/TemplateGeneratorController.php
@@ -1,4 +1,7 @@
 <?php
+
+// require_once '../manager/TemplateGenerator.php';
+
 date_default_timezone_set('Asia/Manila');
 
 $username = $_SESSION['username'];
@@ -7,6 +10,48 @@ $userid = $_SESSION['currentuser'];
 $data = fetchData($userid);
 
 function fetchData($currentuser='') {
+	$conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
+	// $template_generator = new TemplateGenerator();
+	$data = [];
+
+	$current_date = date('Y-m-d H:i:s');
+	$sql = "SELECT 
+		certificate_type, 
+		attendee, 
+		activity_title, 
+		DATE_FORMAT(date_from, '%Y-%m-%d') as date_from, 
+		DATE_FORMAT(date_to, '%Y-%m-%d') as date_to, 
+		activity_venue, 
+		DATE_FORMAT(date_given, '%Y-%m-%d') as date_given, 
+		DATE_FORMAT(date_generated, '%Y-%m-%d') as date_generated,
+		opr
+		FROM template_generator
+		GROUP BY activity_title, date_from, date_to, activity_venue, date_given, date_generated, opr
+		ORDER BY id"; 	
+
+	$query = mysqli_query($conn, $sql);
+
+    while ($row = mysqli_fetch_assoc($query)) {    
+    	
+ 		$data[] = [
+ 			'certificate_type' => $row['certificate_type'],
+ 			'attendee' => $row['attendee'],
+ 			'activity_title' => $row['activity_title'],
+ 			'date_from' => $row['date_from'],
+ 			'date_to' => $row['date_to'],
+ 			'activity_venue' => $row['activity_venue'],
+ 			'date_given' => $row['date_given'],
+ 			'date_generated' => $row['date_generated'],
+ 			'opr' => $row['opr']
+ 		];
+
+    }
+   
+    return $data;
+}
+
+
+function fetchData2($currentuser='') {
 	$conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
 	$data = [];
 
@@ -33,7 +78,6 @@ function fetchData($currentuser='') {
    
     return $data;
 }
-
 
 
 
--- TemplateGenerator/views/table.html.php
+++ TemplateGenerator/views/table.html.php
@@ -2,35 +2,37 @@
 	require_once 'TemplateGenerator/controller/TemplateGeneratorController.php';	
 ?>
 
-<div class="box">
+<div class="box box-primary dropbox">
 	<div class="box-header">
-		<!-- <div class="box-tool"> -->
-			<!-- <div class="margin" style="position:absolute; margin:0px">
-			<div class="btn-group">
-            	<a href='base_tempgen_add_form.html.php?&username=<?php echo $_SESSION['username']; ?>&division=<?php echo $_GET['division']; ?>&emp_id=<?php echo $_SESSION['currentuser']; ?>' class="btn btn-block btn-primary"><i class="fa fa-plus"></i> Create New</a>  
-            </div>
-        	</div> -->
-		<!-- </div> -->
+		
 	</div>
 	<div class="box-body">
 		<div class="margin" style="position:absolute; margin:0px">
 			<div class="btn-group">
-            	<a href='base_template_generator_add_form.html.php?&username=<?php echo $_SESSION['username']; ?>&division=<?php echo $_GET['division']; ?>&emp_id=<?php echo $_SESSION['currentuser']; ?>' class="btn btn-block btn-primary"><i class="fa fa-plus"></i> Create New</a>  
+            	<a href='base_template_generator_add_form.html.php?&username=<?php echo $_SESSION['username']; ?>&division=<?php echo $_GET['division']; ?>&emp_id=<?php echo $_SESSION['currentuser']; ?>' class="btn btn-block btn-primary"><i class="fa fa-plus"></i> Create New</a>
             </div>
+            <!-- <div class="btn-group">
+            	<button href='#' id="view_participants" class="btn btn-block btn-primary view_table" value="participants"><i class="fa fa-plus"></i> View Participants</button>
+            </div>
+            <div class="btn-group">
+            	<button href='#' id="view_main_table" class="btn btn-block btn-primary view_table" value="main_table"><i class="fa fa-plus"></i> Main Table</button>   
+            </div> -->
         </div>
 
 		<table id="list_table" class="table table-striped table-bordered table-responsive table-hover" role="grid">
 			<thead>
 				<tr>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:20%; color: white; background-color: #73758799; border-left: none; border-top-left-radius: 4px; -webkit-border-top-left-radius: 4px; -moz-border-radius-topleft: 4px;">Type</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; color:black; color: white; background-color: #73758799;">Code</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">Title</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%; color:black; color: white; background-color: #73758799;">Attendee</th>
+		              <!-- <th rowspan="2" style = "text-align:center; vertical-align: middle; width:20%; color: white; background-color: #73758799; border-left: none; border-top-left-radius: 4px; -webkit-border-top-left-radius: 4px; -moz-border-radius-topleft: 4px;">Type</th> -->
+		              <!-- <th rowspan="2" style = "text-align:center; vertical-align: middle; color:black; color: white; background-color: #73758799;">Code</th> -->
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:20%; color: white; background-color: #73758799; border-left: none; border-top-left-radius: 4px; -webkit-border-top-left-radius: 4px; -moz-border-radius-topleft: 4px;">Title</th>
+		              <!-- <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%; color:black; color: white; background-color: #73758799;">Attendee</th> -->
 		              <th colspan="2" style = "text-align:center; vertical-align: middle; color:black; color: white; background-color: #73758799;">Activity Date</th> 
 		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:22%; color:black; color: white; background-color: #73758799;">Venue</th> 
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">Date Given</th>
-		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799; border-right: none; border-top-right-radius: 4px; -webkit-border-top-right-radius: 4px; -moz-border-radius-topright: 4px;">Date Generated</th>  
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">Date Issued</th>
 
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">Date Generated</th>
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799;">OPR</th>  
+		              <th rowspan="2" style = "text-align:center; vertical-align: middle; width:10%; color:black; color: white; background-color: #73758799; border-right: none; border-top-right-radius: 4px; -webkit-border-top-right-radius: 4px; -moz-border-radius-topright: 4px;"></th>
 		            </tr>
 		            <tr>
 		              <th style="text-align: center; vertical-align: middle; width:10%; color: white; background-color: #73758799;">From</th>
@@ -40,15 +42,21 @@
 			<tbody id="list_body">
 				<?php foreach ($data as $key => $item): ?>
 					<tr>
-						<td><?php echo $item['certificate_type']; ?></td>
-						<td></td>
+						<!-- <td><?php //echo $item['certificate_type']; ?></td> -->
+						<!-- <td></td> -->
 						<td><?php echo $item['activity_title']; ?></td>
-						<td><?php echo $item['attendee']; ?></td>
+						<!-- <td><?php //echo $item['attendee']; ?></td> -->
 						<td><?php echo $item['date_from']; ?></td>
 						<td><?php echo $item['date_to']; ?></td>
 						<td><?php echo $item['activity_venue']; ?></td>
 						<td><?php echo $item['date_given']; ?></td>
 						<td><?php echo $item['date_generated']; ?></td>
+						<td><?php echo $item['opr']; ?></td>
+						<td>
+							<div class="btn-group">
+				            	<a href='base_tempgen_view_participants.php?&username=<?php echo $_SESSION['username']; ?>&division=<?php echo $_GET['division']; ?>&emp_id=<?php echo $_SESSION['currentuser']; ?>&certificate_type=<?php echo $item['certificate_type']; ?>&activity_title=<?php echo $item['activity_title']; ?>&date_from=<?php echo $item['date_from']; ?>&date_to=<?php echo $item['date_to']; ?>&activity_venue=<?php echo $item['activity_venue']; ?>&date_given=<?php echo $item['date_given']; ?>&date_generated=<?php echo $item['date_generated']; ?>&opr=<?php echo $item['opr']; ?>' class="btn btn-block btn-success view_table" value="participants"><i class="fa fa-users"></i> View Participants</a>
+				            </div>
+						</td>
 					</tr>
 				<?php endforeach ?>
 			</tbody>
@@ -59,13 +67,26 @@
 
 <script type="text/javascript">
 	$(document).ready(function(){
-		let dt = $('#list_table').DataTable( {
+		let dt = $('#list_table').DataTable({
 	      // 'paging'      : true,  
 	      'lengthChange': false,
 	      'searching'   : true,
 	      'ordering'    : false,
 	      'info'        : false,
 	      'autoWidth'   : false,
-	    } );
+	    });
+
+	    $(document).on('click', '.view_table', function(){
+	    	let val = $(this).val();
+	    	if (val == 'participants') {
+	    		$('#list_body').empty();	
+	    		$('#list_table').dataTable().fnClearTable();
+	    		$('#list_table').dataTable().fnDestroy();
+	    		$('#list_table').addClass('hidden');
+
+	    	} else {
+
+	    	}	
+	    });
 	});
 </script>
