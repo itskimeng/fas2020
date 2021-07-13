9c8ee7c7a599fae424e08e33b7c2ef4ad390e8aa
KimPogi <markkimsacluti10101996@gmail.com>
2021-07-13 09:06:05 +0800

    code ref

--- dash_pr.php
+++ dash_pr.php
@@ -133,7 +133,7 @@
                           }
                           ?></td>
                     </tr>
-                  <?php } ?>
+                  <?php } 
               }else{
                 $conn = mysqli_connect("localhost", "fascalab_2020", "w]zYV6X9{*BN", "fascalab_2020");
                   $view_query = mysqli_query($conn, "SELECT * FROM pr where pmo='$user_id' order by id desc ");
@@ -217,7 +217,7 @@
                           }
                           ?></td>
                     </tr>
-                  <?php } ?>
+                  <?php } 
               }
               ?>
             </table>
