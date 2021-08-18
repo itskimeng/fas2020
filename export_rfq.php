822ed946a7f58867814dfe0160a3359135c1c065
KimPogi <markkimsacluti10101996@gmail.com>
2021-08-18 15:00:07 +0800

    RFQ

--- export_rfq.php
+++ export_rfq.php
@@ -137,6 +137,8 @@ while($rowE = mysqli_fetch_assoc($sql_items) ){
   $objPHPExcel->setActiveSheetIndex()->setCellValue('G'.$row,$rowE['qty']);
   $objPHPExcel->setActiveSheetIndex()->setCellValue('H'.$row,$unit);
   $objPHPExcel->setActiveSheetIndex()->setCellValue('I'.$row,number_format($rowE['abc'],2));
+
+
   $objPHPExcel->setActiveSheetIndex()->setCellValue('J'.$row,number_format($total_cost11,2));
 
   // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($stylebottom);
