6c5dc1e6a9b7232d9de7df3c8d1a732021a96a9e
KimPogi <markkimsacluti10101996@gmail.com>
2021-08-24 11:50:16 +0800

    commit dash

--- obligation.php
+++ obligation.php
@@ -45,70 +45,12 @@ if (!isset($_SESSION['username'])) {
   <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
   <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- RIBBON -->
+
+  <link rel="stylesheet" href="0RS/css/ribbon.css">
 <style>
 
 
-/* common */
-.ribbon {
-    width: 150px;
-    height: 150px;
-    overflow: hidden;
-    position: absolute;
-}
-
-.ribbon::before,
-.ribbon::after {
-    position: absolute;
-    z-index: -1;
-    content: '';
-    display: block;
-    border: 5px solid #2980b9;
-}
-
-.ribbon span {
-    position: absolute;
-    display: block;
-    width: 225px;
-    padding: 15px 0;
-    background-color: #3498db;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, .1);
-    color: #fff;
-    font: 700 18px/1 'Lato', sans-serif;
-    text-shadow: 0 1px 1px rgba(0, 0, 0, .2);
-    text-transform: uppercase;
-    text-align: center;
-    left: 50px;
-  top: 100px;
-}
-
-
-/* top right*/
-.ribbon-top-right {
-    top: -10px;
-    right: -10px;
-}
-
-.ribbon-top-right::before,
-.ribbon-top-right::after {
-    border-top-color: transparent;
-    border-right-color: transparent;
-}
-
-.ribbon-top-right::before {
-    top: 0;
-    left: 0;
-}
-
-.ribbon-top-right::after {
-    bottom: 0;
-    right: 0;
-}
-
-.ribbon-top-right span {
-    left: -25px;
-    top: 30px;
-    transform: rotate(45deg);
-}
 </style>
 </head>
 
@@ -124,7 +66,6 @@ if (!isset($_SESSION['username'])) {
           <li class="active">Obligation</li>
         </ol>
         <!-- ==============================DASH =============================== -->
-        <?php include 'ORS/views/ors_dashboard.php'; ?>
         <?php include 'ORS/views/ors_filter.php'; ?>
 
 
