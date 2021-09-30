eb3dcf6e6aa18a2cdece5c5bb4455c2c6fb6e568
KimPogi <markkimsacluti10101996@gmail.com>
2021-09-30 08:53:52 +0800

    commit ict ta

--- rateServiceForm_save.php
+++ rateServiceForm_save.php
@@ -6,47 +6,42 @@ $timeliness = $_GET['timeliness'];
 $quality = $_GET['quality'];
 $date_rated = date('Y-m-d');
 $control_no = $_GET['cn'];
-if($_GET['flag']==1)
+
+for($a=0;$a < count($_GET['rating']); $a++)
 {
-
-}else{
-    for($a=0;$a < count($_GET['rating']); $a++)
-        {
-            $rating =  $_GET['rating'][$a];
-            $sd =  $_GET['sd'][$a];
-            $SQL_insert1 = "INSERT INTO `tblservice_dimension`(`ID`,`CONTROL_NO`, `SERVICE_DIMENTION`, `RATING_SCALE`) VALUES (null,'$control_no','$sd','$rating')";
-            if (mysqli_query($conn, $SQL_insert1)) {
-            } else {
-            }
-        }
-
-// =============================================
-        $sql_select = "SELECT * FROM `tbltechnical_assistance` WHERE CONTROL_NO = '$control_no' ";
-        $result = mysqli_query($conn, $sql_select);
-        while($row = mysqli_fetch_array($result))
-        {
-            $office = $row['OFFICE'];
-            $service = $row['TYPE_REQ'].' ('.$row['TYPE_REQ_DESC'].')';
-            $action_officer = $row['ASSIST_BY'];
-            $suggestion = $_GET['suggestion'];
-            $client = $row['REQ_BY'];
-            $contact_no = $row['CONTACT_NO'];
-            $completed_date = $row['COMPLETED_DATE'];
-
-
-            $SQL_insert2 = "INSERT INTO `tblcustomer_satisfaction_survey`
-            (`ID`, `OFFICE`, `SERVICE_PROVIDED`, 
-            `ACTION_OFFICER`, `SURVEY_MODE`,`SD_ID`, `SUGGESTION`, 
-            `CLIENT`, `CONTACT_NO`, `DATE_ACCOMPLISHED`) 
-            VALUES (null,'$office','$service','$action_officer','Electronics','$control_no','$suggestion','$client','$contact_no','$completed_date')";
-                if (mysqli_query($conn, $SQL_insert2)) {
-                } else {
-                }
-
-                echo $SQL_insert2;
-        }
+    $rating =  $_GET['rating'][$a];
+    $sd =  $_GET['sd'][$a];
+    $SQL_insert1 = "INSERT INTO `tblservice_dimension`(`ID`,`CONTROL_NO`, `SERVICE_DIMENTION`, `RATING_SCALE`) VALUES (null,'$control_no','$sd','$rating')";
+    if (mysqli_query($conn, $SQL_insert1)) {
+    } else {
+    }
 }
 
+// =============================================
+$sql_select = "SELECT * FROM `tbltechnical_assistance` WHERE CONTROL_NO = '$control_no' ";
+$result = mysqli_query($conn, $sql_select);
+while($row = mysqli_fetch_array($result))
+{
+    $office = $row['OFFICE'];
+    $service = $row['TYPE_REQ'].' ('.$row['TYPE_REQ_DESC'].')';
+    $action_officer = $row['ASSIST_BY'];
+    $suggestion = $_GET['suggestion'];
+    $client = $row['REQ_BY'];
+    $contact_no = $row['CONTACT_NO'];
+    $completed_date = $row['COMPLETED_DATE'];
+
+
+    $SQL_insert2 = "INSERT INTO `tblcustomer_satisfaction_survey`
+    (`ID`, `OFFICE`, `SERVICE_PROVIDED`, 
+    `ACTION_OFFICER`, `SURVEY_MODE`,`SD_ID`, `SUGGESTION`, 
+    `CLIENT`, `CONTACT_NO`, `DATE_ACCOMPLISHED`) 
+    VALUES (null,'$office','$service','$action_officer','Electronics','$control_no','$suggestion','$client','$contact_no','$completed_date')";
+        if (mysqli_query($conn, $SQL_insert2)) {
+        } else {
+        }
+
+        echo $SQL_insert2;
+}
 
 
 
@@ -75,3 +70,4 @@ $insert1 ="UPDATE `ta_monitoring` SET
 if (mysqli_query($conn, $insert1)) {
 } else {
 }
+?>
