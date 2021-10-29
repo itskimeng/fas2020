7e10b081c6609b74117390af1aa51d33c8098bf2
KimPogi <markkimsacluti10101996@gmail.com>
2021-10-29 10:04:43 +0800

    code ref

--- template/function.php
+++ template/function.php
@@ -26,7 +26,7 @@ function notification($username)
 {
   include 'connection.php';
   if ($username == 'ljbanalan' || $username == 'mmmonteiro' || $username == 'masacluti' || $username == 'seolivar' || $username == 'jsodsod' || $username== 'aoiglesia' || $username== 'jecastillo' ) { 
-    $query = "SELECT count(*) as 'count' from tbltechnical_assistance where `STATUS_REQUEST` = 'Submitted' and REQ_BY !='' ";
+    $query = "SELECT count(*) as 'count' from tbltechnical_assistance where `STATUS_REQUEST` = 'Submitted' and REQ_BY ='' ";
   }else{ 
   $query = "SELECT count(*) as 'count' from tbltechnical_assistance where `STATUS_REQUEST` = 'Completed' and REQ_BY  = '$username'  ";
   }
