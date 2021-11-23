a1d1ad39ecf12ac8cfeaf3b41cc61425f44a6cb5
KimPogi <markkimsacluti10101996@gmail.com>
2021-11-23 15:18:44 +0800

    code ref

--- current_month.php
+++ current_month.php
@@ -1,15 +1,8 @@
 <select class="form-control " id="table-filter" style="width: 100%;">
-<option>January</option>
-                                  <option>February</option>
-                                  <option >March</option>
-                                  <option >April</option>
-                                  <option >May</option>
-                                  <option >June</option>
-                                  <option >July</option>
-                                  <option >August</option>
-                                  <option >September</option>
-                                  <option >October</option>
-                                  <option>November</option>
-                                  <option>December</option>
+<option>1st Quarter</option>
+<option>2nd Quarter</option>
+<option>3rd Quarter</option>
+<option>4th Quarter</option>
+                                 
   </select>
   <input type="hidden" id = "selectMonth" value = ""/>
