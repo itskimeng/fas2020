546e57b779c635c660231c58a00b1b53b557017e
KimPogi <markkimsacluti10101996@gmail.com>
2021-12-23 09:32:02 +0800

    pr

---
+++ dash_pr_view.php
@@ -0,0 +1,18 @@
+<?php include('base_call_connect.php'); ?> 
+<?php include('connection.php'); ?> 
+<?php require_once 'bower_components/phpti-master/src/ti.php'; ?>
+
+<?php require_once 'menu_checker.php'; ?> 
+<?php $menuchecker = menuChecker('procurement');
+?> 
+
+<?php include 'base_menu.html.php'; ?>
+
+<?php startblock('title'); ?>
+  Procurement
+<?php endblock('title'); ?>
+
+<?php startblock('content'); ?>
+
+  <?php include('PR/views/index.php'); ?>
+<?php endblock(); ?>
