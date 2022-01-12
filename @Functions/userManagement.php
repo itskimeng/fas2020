aa6549d3f0f46cbb05b4dd93c8e1b6c09ee8c380
itskimeng <markkimsacluti10101996@gmail.com>
2022-01-12 13:49:13 +0800

    code ref

--- @Functions/userManagement.php
+++ @Functions/userManagement.php
@@ -8,19 +8,47 @@ $menuIdHolder = $_GET['menuIdHolder'];
 $menuIdHolder = rtrim($menuIdHolder, ',');
 $menuIdHolder = ltrim($menuIdHolder, ',');
 
-$selectIfExisting = ' SELECT `id` FROM `tbl_module_access` WHERE `user_id` = '.$userId.' ';
-$execIfExisting = $conn->query($selectIfExisting);
-if ($execIfExisting->num_rows>0)
-{
-	$sql = ' UPDATE `tbl_module_access` SET `module_id` = "'.$menuIdHolder.'", `moderator_username` = "'.$username.'", `date_updated` = NOW() WHERE `user_id` = '.$userId.' ';
+$subject =  $menuIdHolder;
+$pattern = '/\b([^,]*45[^,]*)\b/';
+preg_match_all($pattern, $subject, $matches, PREG_SET_ORDER);
+foreach ($matches as $val) {
+    $po_admin = $val[1]; 
 }
-else
+if(isset($po_admin))
 {
-	$sql = ' INSERT INTO `tbl_module_access`(`user_id`, `module_id`, `moderator_username`, `date_updated`) VALUES ("'.$userId.'", "'.$menuIdHolder.'", "'.$username.'", NOW()) ';
+	$selectIfExisting = ' SELECT `id` FROM `tbl_module_access` WHERE `user_id` = '.$userId.' ';
+	$execIfExisting = $conn->query($selectIfExisting);
+	if ($execIfExisting->num_rows>0)
+	{
+		$sql = ' UPDATE `tbl_module_access` SET `module_id` = "'.$menuIdHolder.'", `moderator_username` = "'.$username.'", `date_updated` = NOW(), `access_type` = 2 WHERE `user_id` = '.$userId.' ';
+	}
+	else
+	{
+		$sql = ' INSERT INTO `tbl_module_access`(`user_id`, `module_id`, `moderator_username`, `date_updated`) VALUES ("'.$userId.'", "'.$menuIdHolder.'", "'.$username.'", NOW()) ';
+	}
+
+	$exec = $conn->query($sql);
+	echo "success";
+
+}else{
+	$selectIfExisting = ' SELECT `id` FROM `tbl_module_access` WHERE `user_id` = '.$userId.' ';
+	$execIfExisting = $conn->query($selectIfExisting);
+	if ($execIfExisting->num_rows>0)
+	{
+		$sql = ' UPDATE `tbl_module_access` SET `module_id` = "'.$menuIdHolder.'", `moderator_username` = "'.$username.'", `date_updated` = NOW(), `access_type` = 0 WHERE `user_id` = '.$userId.' ';
+	}
+	else
+	{
+		$sql = ' INSERT INTO `tbl_module_access`(`user_id`, `module_id`, `moderator_username`, `date_updated`) VALUES ("'.$userId.'", "'.$menuIdHolder.'", "'.$username.'", NOW()) ';
+	}
+
+	$exec = $conn->query($sql);
+	echo "success";
+
+
 }
 
-$exec = $conn->query($sql);
-echo "success";
+
 
 
  ?>
