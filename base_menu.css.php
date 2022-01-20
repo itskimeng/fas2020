ef31be579c9dbf75fb3ab21f28a193c62971eed6
itskimeng <markkimsacluti10101996@gmail.com>
2022-01-20 08:50:07 +0800

    code ref

--- base_menu.css.php
+++ base_menu.css.php
@@ -50,6 +50,7 @@
   <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
   <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="GSS/views//backend/css/style.css">
 
     <style>
       #calendar {
--- base_menu.js.php
+++ base_menu.js.php
@@ -19,7 +19,6 @@
 <script src="bower_components/toastr-2.1.4-7/toastr.js"></script>
 <script src="PR/views/backend/js/setting.js"></script>
 <script src="bower_components/chart.js/Chart.js"></script>
-<script src="GSS/views/backend/js/custom.js"></script>
 
 
 
