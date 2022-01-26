e481965963b8e29ce5c8f6fe45d3b493a7111daf
itskimeng <markkimsacluti10101996@gmail.com>
2022-01-26 12:41:42 +0800

    code ref

--- update_employee.php
+++ update_employee.php
@@ -121,17 +121,17 @@
   $municipality11           = $row1["city_title"];
   $cid = $_GET['id'];
 
-  $get_details = mysqli_query($conn,"SELECT * FROM tbl_employee WHERE emp_no = '$EMP_NUMBER1' ");
+  $get_details = mysqli_query($conn,"SELECT * FROM tblemployeeinfo WHERE EMP_N = ".$_GET['id']." ");
   $rowEmp = mysqli_fetch_array($get_details);
-  $pagibig = $rowEmp['pagibig'];
+  $pagibig = $rowEmp['PAGIBIG_N'];
   $pagibig_premium = $rowEmp['pagibig_premium'];
   $tin = $rowEmp['tin'];
-  $philhealth = $rowEmp['philhealth'];
-  $gsis = $rowEmp['gsis'];
+  $philhealth = $rowEmp['PHIL_N'];
+  $gsis = $rowEmp['GSIS_N'];
   $salary = $rowEmp['salary'];
-  $step = $rowEmp['step'];
-  $bir = $rowEmp['bir'];
-  $employment_date1 = $rowEmp['employment_date'];
+  $step = $rowEmp['SALARY_GRADE'];
+  $bir = $rowEmp['TIN_N'];
+  $employment_date1 = $rowEmp['DATE_HIRED'];
   $employment_date = date('m/d/Y',strtotime($employment_date1));
 
   if (isset($_POST['submit'])) {
@@ -237,7 +237,7 @@
       REGION_C='$region', PROVINCE_C='$province', CITYMUN_C='$municipality',
       POSITION_C='$position',PERMANENT_ADDRESS = '$permanent_address', CURRENT_ADDRESS = '$current_address',STATUS_OF_APP = '$office_landline',
       MOBILEPHONE='$cellphone', EMAIL='$email',
-      ALTER_EMAIL='$alter_email',  LANDPHONE='$contact', OFFICE_STATION='$office', DIVISION_C='$division', ACTIVATED='".$e_stats."', UNAME='$username',DESIGNATION='$designation',SUFFIX='$suffix',LANDPHONE='$office_contact',REMARKS_M='$office_address' WHERE EMP_N = '$cid' LIMIT 1");
+      ALTER_EMAIL='$alter_email',  DATE_HIRED ='$employment_date', LANDPHONE='$contact', OFFICE_STATION='$office', DIVISION_C='$division', ACTIVATED='".$e_stats."', UNAME='$username',DESIGNATION='$designation',SUFFIX='$suffix',LANDPHONE='$office_contact',REMARKS_M='$office_address' WHERE EMP_N = '$cid' LIMIT 1");
 
     # code...
     $select = mysqli_query($conn,"SELECT $step1 FROM tbl_salary_grade WHERE salary_grade = '$salary' ");
