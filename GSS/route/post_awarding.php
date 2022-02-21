94659181a506374991e2f0468772b1834dbb962a
itskimeng <markkimsacluti10101996@gmail.com>
2022-02-21 15:47:42 +0800

    Update post_awarding.php

--- GSS/route/post_awarding.php
+++ GSS/route/post_awarding.php
@@ -31,7 +31,8 @@ $award->update(
     ],
     "rfq_no='$rfq_no'"
 );
-header('location: ../../procurement_awarding.php?pr_no='.$_POST['cform-pr-no-awarded'].'&rfq_no='.$_POST['cform-rfq-no-awarded'].'');
+
+header('location:procurement_awarding.php?pr_no='.$_POST['cform-pr-no-awarded'].'&rfq_no='.$_POST['cform-rfq-no-awarded'].'');
 ?>
 
 
