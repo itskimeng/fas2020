ef2f2b808aa19350c635cd1aab4516c8fbcc9c46
Jeck Castillo <58018826+breezelback@users.noreply.github.com>
2022-03-02 20:16:15 +0800

    History

--- Finance/views/BudgetObligation/history.php
+++ Finance/views/BudgetObligation/history.php
@@ -91,6 +91,35 @@
 
 
 <div class="content-wrapper">
+
+  <div class="box box-warning dropbox">
+    <div class="box-body">
+     <div class="row">
+        <div class="col-md-3">
+          <?php echo group_daterange3('Date', 'timeline', 'timeline', '', '', 'daterange ', 1, false); ?>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="btn-group">
+            <a href="javascript:void(0);" class="btn btn-primary btn-filter"><i class="fa fa-filter"></i> Filter</a>
+          </div>
+
+          <div class="btn-group">
+            <a href="javascript:void(0);" class="btn btn-warning" disabled><i class="fa fa-download"></i> Export</a>
+          </div>
+
+          <div class="btn-group">
+            <a href="cash_payment_new.php" class="btn btn-success"><i class="fa fa-plus"></i> Create</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  
   <section class="content-header">
     <h1>ORS/BURS</h1>
     
