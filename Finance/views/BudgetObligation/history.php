e751206b31c8c91afb83aa3ddf88710f8c1ad19d
Jeck Castillo <58018826+breezelback@users.noreply.github.com>
2022-03-02 20:20:48 +0800

    Update history.php

--- Finance/views/BudgetObligation/history.php
+++ Finance/views/BudgetObligation/history.php
@@ -92,32 +92,6 @@
 
 <div class="content-wrapper">
 
-  <div class="box box-warning dropbox">
-    <div class="box-body">
-     <div class="row">
-        <div class="col-md-3">
-          <?php echo group_daterange3('Date', 'timeline', 'timeline', '', '', 'daterange ', 1, false); ?>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-12">
-          <div class="btn-group">
-            <a href="javascript:void(0);" class="btn btn-primary btn-filter"><i class="fa fa-filter"></i> Filter</a>
-          </div>
-
-          <div class="btn-group">
-            <a href="javascript:void(0);" class="btn btn-warning" disabled><i class="fa fa-download"></i> Export</a>
-          </div>
-
-          <div class="btn-group">
-            <a href="cash_payment_new.php" class="btn btn-success"><i class="fa fa-plus"></i> Create</a>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
 
   
   <section class="content-header">
@@ -131,6 +105,17 @@
     </ol> 
   </section>
   <section class="content">
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box box-warning dropbox">
+          <div class="box-body">
+            <a href="budget_obligation.php?page=1" class="btn btn-primary"><i class="fa fa-times"></i> Close</a>
+
+          </div>
+        </div>
+      </div>
+    </div>
     
     <div class="row">
       <div class="col-md-4">
