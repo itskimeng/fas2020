de10ae22898c5cd8e0ee6ba0d86e1e705ab384bf
itskimeng <markkimsacluti10101996@gmail.com>
2022-03-31 16:44:34 +0800

    ccc

--- GSS/manager/GSSManager.php
+++ GSS/manager/GSSManager.php
@@ -511,7 +511,7 @@ class GSSManager  extends Connection
         LEFT JOIN tbl_pr_status as ps on ps.id = pr.stat
         LEFT JOIN tblemployeeinfo emp ON pr.received_by = emp.EMP_N
         LEFT JOIN tbl_pr_type pt on pt.id = pr.type
-        where YEAR(date_added) = '2022' 
+        where YEAR(date_added) = '2022'  and pr_no != ''
         GROUP BY items.pr_no
         order by pr.id desc";
 
@@ -740,11 +740,11 @@ class GSSManager  extends Connection
             $str = str_replace($year . "-" . $current_month . "-", "", $row['count_r']);
             if ($row['count_r'] == 1) {
                 $idGet = (int)$str + 1;
-                $pr_no = $year . '-' . $current_month . '-' . '000' . $idGet;
+                $pr_no = $year . '-' . $current_month . '-' . '00' . $idGet;
             } else if ($row['count_r'] <= 99) {
                 $idGet = (int)$str + 1;
 
-                $pr_no = $year . '-' . $current_month . '-' . '000' . $idGet;
+                $pr_no = $year . '-' . $current_month . '-' . '00' . $idGet;
             } else {
                 $idGet = (int)$str + 1;
 
