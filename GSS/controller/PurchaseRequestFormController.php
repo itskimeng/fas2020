256bcdd474940601b0cef7f68907a51ab5a08bbf
itskimeng <markkimsacluti10101996@gmail.com>
2022-04-06 11:33:07 +0800

    code ref

--- GSS/controller/PurchaseRequestFormController.php
+++ GSS/controller/PurchaseRequestFormController.php
@@ -26,5 +26,9 @@ $get_pr         = $gm->fetchPrNo('2022');//CREATE PR
 $get_pr_id      = $gm->fetchID();//CREATE PR
 $pmo            = $gm->getPMO();//CREATE PR
 $pr_items       = $gm->view_pr_items($id);//CREATE PR
+$pr_data        = $gm->view_pr($id);//view pr
+$type_opt       = $gm->fetchType();//view pr
+
+
 ?>
 
