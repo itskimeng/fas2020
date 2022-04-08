e6b30f60b82b1f3b7c1df1fd88adf79129aef843
itskimeng <markkimsacluti10101996@gmail.com>
2022-04-08 13:56:17 +0800

    code ref

--- GSS/controller/PurchaseRequestFormController.php
+++ GSS/controller/PurchaseRequestFormController.php
@@ -5,6 +5,7 @@ date_default_timezone_set('Asia/Manila');
 require 'GSS/manager/GSSManager.php';
 $office = $_GET['division'];
 $admin = ['masacluti','jsodsod','jecastillo','cmfiscal','mmmonteiro'];
+$flag='';
 if(isset($_GET['id']) )
 {
     $id=$_GET['id'];
@@ -13,10 +14,12 @@ if(isset($_GET['id']) )
         $id = $_GET['pr_no'];
     }else{
     $id=$_GET['id'];
+    $flad=0;
 
     }
 }else if(isset($_GET['pr_no'])){
     $id = $_GET['pr_no'];
+    $flag=1;
 }
 $pmo_id = $_GET['division'];  
 
@@ -25,7 +28,7 @@ $route = 'GSS/route/';
 $get_pr         = $gm->fetchPrNo('2022');//CREATE PR
 $get_pr_id      = $gm->fetchPRID($_GET['pr_no']);//CREATE PR
 $pmo            = $gm->getPMO();//CREATE PR
-$pr_items       = $gm->view_pr_items($id);//CREATE PR
+$pr_items       = $gm->view_pr_items($id,$flag);//CREATE PR
 $pr_data        = $gm->view_pr($id);//view pr
 $type_opt       = $gm->fetchType();//view pr
 $pr             = $gm->fetch_abc($_GET['pr_no']);
