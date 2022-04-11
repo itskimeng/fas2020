8f1777a429e2dd7f94819c64ec62fc1163a396f1
itskimeng <markkimsacluti10101996@gmail.com>
2022-04-11 16:36:50 +0800

    a

--- GSS/views/PR/index.php
+++ GSS/views/PR/index.php
@@ -1,5 +1,11 @@
 
+<div id="overlay">
+<img src="images/loading.gif" style=" position: fixed; left: 700px; top:250px; z-index: 9999;" /> 
+</div>
 
+<script>
+  $('#overlay').fadeOut(5000);
+</script>
 <?php require_once 'GSS/controller/PurchaseRequestController.php'; ?>
 <?php $menuchecker = menuChecker('procurement'); ?>
 <div class="content-wrapper">
