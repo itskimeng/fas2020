dd1fc63eff79850c1361bacc7ec6e3e5e5076203
itskimeng <markkimsacluti10101996@gmail.com>
2022-04-28 16:12:57 +0800

    rfq

---
+++ procurement_request_for_quotation_edit.php
@@ -0,0 +1,22 @@
+<?php include('base_call_connect.php'); ?> 
+<?php include('connection.php'); ?> 
+<?php include('GSS/macro/macro.php'); ?> 
+
+<?php require_once 'bower_components/phpti-master/src/ti.php'; ?>
+
+<link href='GSS/views/backend/css/buttons.css' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+<?php require_once 'menu_checker.php'; ?> 
+<?php $menuchecker = menuChecker('rfq_form_edit');?> 
+
+<?php include 'base_menu.html.php'; ?>
+
+<?php startblock('title'); ?>
+  Procurement
+<?php endblock('title'); ?>
+
+<?php startblock('content'); ?>
+
+<?php include ('GSS/views/RFQ/form/rfq_edit.php'); ?>
+<?php endblock(); 
