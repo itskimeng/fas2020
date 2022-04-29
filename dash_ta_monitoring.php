b3124fac9bc81fe5c93f4dc2372d1fde82405853
itskimeng <markkimsacluti10101996@gmail.com>
2022-04-29 15:23:14 +0800

    ict

---
+++ dash_ta_monitoring.php
@@ -0,0 +1,29 @@
+
+
+<?php include('base_call_connect.php'); ?> 
+<?php include('connection.php'); ?> 
+<?php include('GSS/macro/macro.php'); ?> 
+<?php require_once 'menu_checker.php'; ?> 
+
+<?php $menuchecker = menuChecker('monitoring');?>
+
+<?php require_once 'bower_components/phpti-master/src/ti.php'; ?>
+
+<link href='GSS/views/backend/css/buttons.css' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+<?php require_once 'menu_checker.php'; ?> 
+
+
+<?php include 'base_menu.html.php'; ?>
+
+<?php startblock('title'); ?>
+  Procurement
+<?php endblock('title'); ?>
+
+<?php startblock('content'); ?>
+
+<?php include ('ict/views/monitoring/index.php'); ?>
+<?php include ('macro/macro.php'); ?>
+<?php endblock(); ?>
+
