9501584553ac05cef11a993649f9ecbbcb47b6a5
itskimeng <markkimsacluti10101996@gmail.com>
2022-05-05 10:31:50 +0800

    ict ta

--- ict/manager/ICTManager.php
+++ ict/manager/ICTManager.php
@@ -23,7 +23,7 @@ class ICTManager  extends Connection
 
     public function fetch()
     {
-        $sql = "SELECT * from $this->default_table";
+        $sql = "SELECT * from $this->default_table where REQ_DATE >= '2021-06-15'";
         $query = $this->db->query($sql);
         $data = [];
 
