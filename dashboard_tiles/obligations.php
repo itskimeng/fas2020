ed78df6ecf9126cfa92b08497027ae0bbd97a184
sunonin <sodsodjomarie@gmail.com>
2022-05-12 10:30:39 +0800

    update

--- dashboard_tiles/obligations.php
+++ dashboard_tiles/obligations.php
@@ -1,12 +1,17 @@
 <?php 
+  require_once 'Finance/manager/BudgetManager.php';
+
   $dashboard = new Dashboard();
+  $bm = new BudgetManager();
+
   $obligations = $dashboard->getObligations();
+  $ors_data = $bm->getObligationsData();
 ?>
 
 <div class="col-md-4 col-sm-4 col-xs-12">
   <div class="info-box">
     <div class="panel-heading bg-blue"><i class="fa fa-gavel"></i> <b>OBLIGATION</b>
-      <a href="obligation.php?page=1&ipp=10&division=<?php echo $_GET['division'];?>" class="pull-right btn btn-success btn-xs"><i class="fa fa-folder-open"></i> VIEW ALL</a>
+      <a href="budget_obligation.php?page=1&ipp=10&division=<?php echo $_GET['division'];?>" class="pull-right btn btn-success btn-xs"><i class="fa fa-folder-open"></i> VIEW ALL</a>
       <div class="clearfix"></div>
     </div>
     <div class="obligations" id="row5" style="height: 394px; overflow-y: hidden;">
@@ -20,22 +25,20 @@
           </tr>
         </thead>
         <tbody>
-          <?php foreach ($obligations as $key => $obligation): ?>
-            <tr>
-              <td>
-                <?php echo $obligation['ors']; ?>
-              </td>
-              <td>
-                <?php echo $obligation['particular']; ?>  
-              </td>
-              <td style='background-color:#e19292; color:white;'>
-                <?php if ($obligation['status'] =='Pending'): ?>
-                  <b>Pending</b>
-                <?php elseif ($obligation['status'] == 'Obligated'): ?>
-                  <b>Obligated</b>
-                <?php endif ?>
-              </td>
-            </tr> 
+          <?php foreach (array_slice($ors_data['normal'], 0, 8) as $key => $ors): ?>
+            <?php if (!empty($ors['particular'])): ?>
+              <tr>
+                <td>
+                  <span class="badge bg-info"><?= $ors['serial_no']; ?></span>
+                </td>
+                <td>
+                  <?= $ors['particular']; ?>  
+                </td>
+                <td style='background-color:#e19292; color:white;'>
+                  <?= $ors['status']; ?>
+                </td>
+              </tr> 
+            <?php endif ?>
           <?php endforeach ?>
         </tbody>
         </table>  
