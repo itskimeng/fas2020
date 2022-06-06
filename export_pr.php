716afb65b76066e6afe067b39a4774eb83884488
itskimeng <markkimsacluti10101996@gmail.com>
2022-06-06 14:53:08 +0800

    code ref

--- export_pr.php
+++ export_pr.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 
 
 
-    $query = mysqli_query($conn, "SELECT SUM(pr.qty * pr.abc) as total FROM pr_items pr LEFT JOIN app on app.id = pr.items LEFT JOIN item_unit item on item.id = pr.unit WHERE id =  '$id' ");
+    $query = mysqli_query($conn, "SELECT SUM(pr.qty * pr.abc) as total FROM pr_items pr LEFT JOIN app on app.id = pr.items LEFT JOIN item_unit item on item.id = pr.unit WHERE pr.pr_id =  '$id' ");
     $row_q = mysqli_fetch_array($query);
     $total_abc = $row_q['total'];
     
@@ -52,7 +52,7 @@ if (in_array($pmo, $fad)) {
 }
 
 
-$sql_items = mysqli_query($conn, "SELECT a.sn, a.id, a.procurement, pr.description, iu.item_unit_title, pr.qty, pr.abc FROM pr_items pr LEFT JOIN app a ON a.id = pr.items LEFT JOIN item_unit iu on iu.id = pr.unit WHERE pr.id = '$pr_no' ");
+$sql_items = mysqli_query($conn, "SELECT a.sn, a.id, a.procurement, pr.description, iu.item_unit_title, pr.qty, pr.abc FROM pr_items pr LEFT JOIN app a ON a.id = pr.items LEFT JOIN item_unit iu on iu.id = pr.unit WHERE pr.pr_id = '$pr_no' ");
 // $sql_items = mysqli_query($conn, "SELECT a.sn,a.id,a.procurement,pr.description,pr.unit,pr.qty,pr.abc FROM pr_items pr left join app a on a.id = pr.items WHERE pr.pr_no = '$pr_no' ");
 if (mysqli_num_rows($sql_items)>30) {
   
