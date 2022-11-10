0180895512edcf38b13f704010b9ee0bdced77cb
itskimeng <markkimsacluti10101996@gmail.com>
2022-11-10 16:06:59 +0700

    code ref

--- GSS/manager/GSSManager.php
+++ GSS/manager/GSSManager.php
@@ -1470,11 +1470,45 @@ if(!empty($office)){
         $data = [];
 
         while ($row = mysqli_fetch_assoc($query)) {
+            $office = $row['pmo'];
+            $fad = ['10', '11', '12', '13', '14', '15', '16'];
+            $ord = ['1', '2', '3', '5'];
+            $lgmed = ['7', '18', '7',];
+            $lgcdd = ['8', '9', '17', '9'];
+            $cavite = ['20', '34', '35', '36', '45'];
+            $laguna = ['21', '40', '41', '42', '47', '51', '52'];
+            $batangas = ['19', '28', '29', '30', '44'];
+            $rizal = ['23', '37', '38', '39', '46', '50'];
+            $quezon = ['22', '31', '32', '33', '48', '49', '53'];
+            $lucena_city = ['24'];
+            if (in_array($office, $fad)) {
+                $office = 'FAD';
+            } else if (in_array($office, $lgmed)) {
+                $office = 'LGMED';
+            } else if (in_array($office, $lgcdd)) {
+                $office = 'LGCDD';
+            } else if (in_array($office, $cavite)) {
+                $office = 'CAVITE';
+            } else if (in_array($office, $laguna)) {
+                $office = 'LAGUNA';
+            } else if (in_array($office, $batangas)) {
+                $office = 'BATANGAS';
+            } else if (in_array($office, $rizal)) {
+                $office = 'RIZAL';
+            } else if (in_array($office, $quezon)) {
+                $office = 'QUEZON';
+            } else if (in_array($office, $lucena_city)) {
+                $office = 'LUCENA CITY';
+            } else if (in_array($office, $ord)) {
+                $office = 'ORD';
+            }else{
+                $office = '~';
+            }
 
             $data[] = [
                 'id' => $row['id'],
                 'pr_no' => $row['pr_no'],
-                'pmo' => $row['pmo'],
+                'pmo' => $office,
                 'purpose' => $row['purpose'],
                 'pr_date' => date('F d, Y', strtotime($row['pr_date'])),
                 'total_abc' => '₱ '.number_format($row['total_abc'],2),
---
+++ GSS/views/PR/modal/modal_pending_pr.php
@@ -0,0 +1,67 @@
+<div class="modal fade" id="pendingModal" style="display: none;">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content" style="border-radius:20px;width:100%">
+            <div class="modal-header">
+
+                
+                <div id="box">
+                <h4 class="modal-title"><i>List of
+                        Unfinished Purchase
+                        Request</i></h4>
+                    <div id="inside_box" class="w3-panel w3-pale-red w3-border w3-leftbar w3-border-red"
+                        style="border-radius:20px;">
+                        <h3><img src="images/pending.png" style="width:40px;height:40px" /> REMINDER!</h3>
+                        <div>Please complete your unfinished purchase request to make another one</div><br>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-body">
+                <table id="pendingTable" class="table table-bordered table-striped" role="grid">
+                    <thead>
+                        <tr>
+                            <th class="text-center">PR NUMBER</th>
+                            <th class="text-center">OFFICE</th>
+                            <th class="text-center">PURPOSE</th>
+                            <th class="text-center">PRICE</th>
+                            <th class="text-center">PR DATE</th>
+                            <th class="text-center">ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tbody-pending">
+                        <?php foreach($pending_pr as $key => $item):?>
+                        <tr>
+                            <td><?= $item['pr_no'];?></td>
+                            <td><?= $item['pmo'];?></td>
+                            <td><?= $item['purpose'];?></td>
+                            <td><?= $item['total_abc'];?></td>
+                            <td><?= $item['pr_date'];?></td>
+                            <td style="background-color:;" class=" text-center">
+                                <button class="btn btn-success btn-sm btn-view" title="View">
+                                    <a
+                                        href="procurement_purchase_request_view.php?division=&amp;id=<?= $item['id'];?>&amp;pr_no=<?= $item['pr_no'];?>">
+                                        <i class="fa fa-eye"></i>
+                                    </a>
+                                </button>
+                                <button class="btn btn-danger btn-sm btn-view">
+                                    <a
+                                        href="GSS/route/post_to_budget.php?quarter=<?= $_GET['quarter'];?>&amp;division=&amp;pr_no=<?= $item['pr_no'];?>&amp;id=<?= $item['id'];?>">
+                                        <i class="fa fa-share-square"></i>
+                                    </a>
+                                </button>
+                                <button id="btn_submit_to_gss" disabled="" class="btn btn-primary btn-sm btn-view"
+                                    title="Submit to GSS" data-id="<?= $item['id'];?>" value="<?= $item['pr_no'];?>">
+                                    <i class="fa fa-send"></i>
+                                </button>
+                                <button id="sweet-4" class="btn btn-warning btn-sm btn-view" title="Cancel this PR"
+                                    data-id="<?= $item['id'];?>" value="<?= $item['pr_no'];?>">
+                                    <i class="fa fa-times-circle"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
