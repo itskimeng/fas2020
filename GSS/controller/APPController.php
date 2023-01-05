212249d88b18545f3a24483ab90c84a94fbc1bc5
itskimeng <markkimsacluti10101996@gmail.com>
2023-01-05 14:48:46 +0800

    SHET

--- GSS/controller/APPController.php
+++ GSS/controller/APPController.php
@@ -8,13 +8,13 @@ require_once 'GSS/manager/GSSManager.php';
 $data = new GSSManager();
 $division = $_GET['division'];
 $id = $_GET['id'];
-$admins = ['masacluti','cmfiscal','sglee','epalforja','jsodsod','jecastillo'];
+$admins = ['masacluti','cmfiscal','jedeleon'];
 
 $path = 'GSS/route/';
 $app_sn = $data->fetch();
 $pmo_list = $data->setPMO();
 $app_pmo_list = $data->setAppPMO();
-$app = $data->fetchAPP($admins);
+$app = $data->fetchAPP($admins,$_GET['year']);
 
 $app_category = $data->setCategory();
 $pages = $data->setPages();
@@ -22,7 +22,8 @@ $app_unit = $data->getItemUnit();
 $app_item_unit = $data->getAppItemUnit();
 $app_sf = $data->getSF();
 $app_mode = $data->getMode();
-$app_item_list = $data->getAPPItemList('2022');
+$app_item_list = $data->getAPPItemList('2023');
+
 $app_opts = $data->getAPP();
 // $app_type = ['1' => 'Catering Services', '2' => 'Meals, Venue and Accommodation','3' => 'Repair and Maintenance','4' => 'Supplies, Materials and Devices','5' => 'Other Services','6' => 'Reimbursement and Petty Cash'];
 $app_type           =       $data->fetchModeofProc();
