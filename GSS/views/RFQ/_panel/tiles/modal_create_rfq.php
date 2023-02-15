377a399594c0cea9db1bd18a986a1ab42e67b558
itskimeng <markkimsacluti10101996@gmail.com>
2023-02-15 15:08:21 +0800

    modal rfq

--- GSS/views/RFQ/_panel/tiles/modal_create_rfq.php
+++ GSS/views/RFQ/_panel/tiles/modal_create_rfq.php
@@ -23,7 +23,7 @@
                                             <!-- Color Picker -->
                                             <div class="form-group">
                                                 <label>RFQ No:</label>
-                                                <input type="text" class="form-control pull-right" value="<?= $rfq_no['rfq_no']; ?>" name="rfq_no">
+                                                <input type="text" class="form-control pull-right" value="<?= $rfq_no['rfq_no']; ?>" name="rfq_no" readonly>
 
                                             </div>
                                             <!-- /.form group -->
