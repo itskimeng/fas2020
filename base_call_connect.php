3bb6f447a468ceb2ff64fcc780b60e1d09f031b2
itskimeng <markkimsacluti10101996@gmail.com>
2023-03-21 11:12:37 +0800

    code ref

--- base_call_connect.php
+++ base_call_connect.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['username'])) {
 } else {
 	error_reporting(0);
 	ini_set('display_errors', 0);
-	$admins = ['charlesodi', 'mmmonteiro', 'cvferrer', 'masacluti', 'seolivar'];
+	$admins = ['charlesodi', 'mmmonteiro', 'cvferrer', 'masacluti', 'seolivar','ljbanalan'];
 	$data['username'] = $_SESSION['username'];
 	$data['division'] = $_GET['division'];
 	$data['DEPT_ID'] = $_SESSION['DEPT_ID'];
