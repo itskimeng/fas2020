25c1d0749f380f7b0049b8b760b8b6768cf4a92f
itskimeng <markkimsacluti10101996@gmail.com>
2023-03-24 11:30:44 +0800

    code ref

--- ICTTechnicalAssistance/views/base_user_ict_dashboard.php
+++ ICTTechnicalAssistance/views/base_user_ict_dashboard.php
@@ -28,54 +28,17 @@
       </div>
 
       <div class="col-md-12">
-        <div class="col-lg-3">
+      <div class="col-lg-12">
           <div class="box box-warning dropbox">
-            <div class="box-header">
-              <h3 class="box-title" style="color:black!important;"><i class="fa fa-graph"></i>ICT Staff Work Load</h3>
-            </div>
-            <div class="box-body custom-box-body">
-               
-            <?php foreach ($rictu_opts as $key => $data):?>
-                
-                <div class="list-group contact-group zoom" style="margin-bottom: 5px;">
-                  <a href="#" class="list-group-item" style="padding: 7px 7px; background-color:#f3eff5">
-                    <div class="media">
-                      <div class="pull-left" style="width:65px; height:65px;">
-                        <img class="img-circle" style="border-radius: 5px; width: 100%; height: 100%; object-fit: cover;" src="images/profile/<?= $data['rictu_staff'][$key];?>.jpg" alt="...">
-                      </div>
-                      <div class="media-body">
-                        <div class="media-content">
-                          <small><?= $data['position'][$key];?></small><br>
-                        </div>
-
-                        <div class="media-content" style="margin-top: -1%;">
-                          <i class="fa fa-user"></i> <b class="media-heading" style="font-size: 10pt;"><?= $data['rictu_staff'][$key];?></b>
-                        </div>
-
-                        <div class="media-content" style="margin-top: -1%;">
-                          <small><i class="fa fa-envelope"></i><?= $data['email_address'][$key];?></small>
-                        </div>
-                        <div class="media-content" style="margin-top: -2%;">
-                          <small><i class="fa fa-phone"></i><?= $data['contact_details'][$key];?></small>
-                          <ul class="list-unstyled pull-right">
-                            <span class="label label-warning label2"><?= count($workload['ongoing'][$key]);?></span>
-                            <span class="label label-primary label2"><?= count($workload['completed'][$key]);?></span>
-                            <span class="label label-success label2"><?= count($workload['for rating'][$key]);?></span>
-                          </ul>
-                        </div>
-
-
-
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              <?php endforeach;?>
+            
+            <div class="box-body custom-box-body" >
+          <button id="btn_create" class="btn-lg  btn-default" style="background:linear-gradient(90deg,#64B5F6,#0D47A1);color:#fff;">Create ICT Request</button>
+      
             </div>
           </div>
-          <button id="btn_create" class="btn-lg col-lg-12 btn-default" style="background:linear-gradient(90deg,#64B5F6,#0D47A1);color:#fff;">Create ICT Request</button>
+         
         </div>
-        <div class="col-lg-9">
+        <div class="col-lg-12">
           <?php include('tiles/ict_monitoring.html.php'); ?>
 
         </div>
