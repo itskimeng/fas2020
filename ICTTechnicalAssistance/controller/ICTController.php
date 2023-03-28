166415aea7d89ba20084d348d22f6a4536c44bc9
itskimeng <markkimsacluti10101996@gmail.com>
2023-03-28 16:22:57 +0800

    code ref

--- ICTTechnicalAssistance/controller/ICTController.php
+++ ICTTechnicalAssistance/controller/ICTController.php
@@ -11,7 +11,7 @@ $default_year = '2023';
 $ict = new ICTTechAssistanceManager();
 
 $tasks      = ($_GET['role'] == '21232f297a57a5a743894a0e4a801fc3') ? $ict->getData(null,$_GET['quarter'],$default_year) : $ict->getData($_SESSION['currentuser'],$_GET['quarter'],$default_year);   //$ict->getData($_GET['quarter'],$default_year);
-$ict_opts   = ($_GET['role'] == '21232f297a57a5a743894a0e4a801fc3') ? $ict->monitoringTable($_GET['role'])  : $ict->monitoringTable($_SESSION['complete_name2']);
+$ict_opts   = ($_GET['role'] == '21232f297a57a5a743894a0e4a801fc3') ? $ict->monitoringTable($_GET['role'])  : $ict->monitoringTable($_SESSION['currentuser']);
 $rictu_opts = $ict->fetchRICTUDetails();
 $workload   = $ict->fetchWorkLoad();
 
