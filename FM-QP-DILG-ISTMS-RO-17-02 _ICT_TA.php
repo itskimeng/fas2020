b4d2771eb3e059ab9cd9aed1f81e23d9bbbe911f
itskimeng <markkimsacluti10101996@gmail.com>
2023-04-14 09:51:32 +0800

    fixed the sql query

--- FM-QP-DILG-ISTMS-RO-17-02 _ICT_TA.php
+++ FM-QP-DILG-ISTMS-RO-17-02 _ICT_TA.php
@@ -115,7 +115,7 @@ if($month == '1' || $month == 0)
 // exit();
 $objPHPExcel->setActiveSheetIndex(0)->setCellValue('C7',$quarter);
 
-$sql_q10 = mysqli_query($conn, "SELECT MONTHNAME(`REQ_DATE`) AS 'month', YEAR(`REQ_DATE`) AS 'year', `ID`, `CONTROL_NO`, `REQ_DATE`, `REQ_TIME`,emp.FIRST_M, emp.LAST_M, `REQ_BY`, `OFFICE`, `POSITION`, `CONTACT_NO`, `EMAIL_ADD`, `EQUIPMENT_TYPE`, `BRAND_MODEL`, `PROPERTY_NO`, `SERIAL_NO`, `IP_ADDRESS`, `MAC_ADDRESS`, `TYPE_REQ`, `TYPE_REQ_DESC`, `ISSUE_PROBLEM`, `START_DATE`, `START_TIME`, `STATUS_DESC`, `COMPLETED_DATE`, `COMPLETED_TIME`, `ASSIST_BY`, `PERSON_ASSISTED`, `TIMELINESS`, `QUALITY`, `STATUS` FROM `tbltechnical_assistance`  LEFT JOIN tblemployeeinfo emp on tbltechnical_assistance.REQ_BY = emp.EMP_N WHERE MONTH(REQ_DATE) IN $months and YEAR(REQ_DATE) = $year and `REQ_DATE` != '0000-00-00'   order by  `CONTROL_NO`");
+$sql_q10 = mysqli_query($conn, "SELECT MONTHNAME(`REQ_DATE`) AS 'month', YEAR(`REQ_DATE`) AS 'year', `ID`, `CONTROL_NO`, `REQ_DATE`, `REQ_TIME`,emp.FIRST_M, emp.LAST_M, `REQ_BY`, `OFFICE`, `POSITION`, `CONTACT_NO`, `EMAIL_ADD`, `EQUIPMENT_TYPE`, `BRAND_MODEL`, `PROPERTY_NO`, `SERIAL_NO`, `IP_ADDRESS`, `MAC_ADDRESS`, `TYPE_REQ`, `TYPE_REQ_DESC`, `ISSUE_PROBLEM`, `START_DATE`, `START_TIME`, `STATUS_DESC`, `COMPLETED_DATE`, `COMPLETED_TIME`, `ASSIST_BY`, `PERSON_ASSISTED`, `TIMELINESS`, `QUALITY`, tbltechnical_assistance.`STATUS` FROM `tbltechnical_assistance`  LEFT JOIN tblemployeeinfo emp on tbltechnical_assistance.REQ_BY = emp.EMP_N WHERE MONTH(REQ_DATE) IN $months and YEAR(REQ_DATE) = $year and `REQ_DATE` != '0000-00-00'   order by  `CONTROL_NO`");
  
 if (mysqli_num_rows($sql_q10)>0) {
     $row = 10;
