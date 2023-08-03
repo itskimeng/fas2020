4dfea78f3925758dcbc2c37aa0ef27af2bf40396
itskimeng <markkimsacluti10101996@gmail.com>
2023-08-03 08:56:21 +0800

    code ref

--- sidebar2.php
+++ sidebar2.php
@@ -28,7 +28,7 @@
   <div class="wrapper">
   <?php include('template/header.php');?>
     <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar"  style = "background-color:#f6cdd0;">
+    <aside class="main-sidebar" >
       <!-- sidebar: style can be found in sidebar.less -->
       <section class="sidebar">
         <!-- Sidebar user panel -->
